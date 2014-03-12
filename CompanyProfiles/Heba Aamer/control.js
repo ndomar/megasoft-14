747a598db5a9a5b971b4d6f79af3ee39265a034a
heba-aamer <heba93aamer@gmail.com>
2014-03-12 10:03:50 +0200

    adding link

--- CompanyProfiles/Heba Aamer/control.js
+++ CompanyProfiles/Heba Aamer/control.js
@@ -1,17 +1,36 @@
 function myFunction1() {
-document.getElementById("A").innerHTML ="- I am a 6th semester CSEN student at GUC.<br>- Currently I am JTA in the <br>    concepts of programming languages course. <br>- I love sports in general. ";
+var x = document.getElementById("A");
+if(x.innerHTML == "- I am a 6th semester CSEN student at GUC.<br>- Currently I am JTA in the <br>    concepts of programming languages course. <br>- I love sports in general. ") {
+x.innerHTML="";
+} else {
+x.innerHTML ="- I am a 6th semester CSEN student at GUC.<br>- Currently I am JTA in the <br>    concepts of programming languages course. <br>- I love sports in general. ";
+}
 document.getElementById("B").innerHTML="";
 document.getElementById("C").innerHTML="";
 }
 
 function myFunction2() {
 document.getElementById("A").innerHTML="";
-document.getElementById("B").innerHTML="Employee in Component 1.";
+var x = document.getElementById("B");
+if(x.innerHTML == "Employee in Component 1.") {
+x.innerHTML="";
+} else {
+x.innerHTML="Employee in Component 1.";
+}
 document.getElementById("C").innerHTML="";
 }
 
 function myFunction3() {
 document.getElementById("A").innerHTML="";
 document.getElementById("B").innerHTML="";
-document.getElementById("C").innerHTML="<ul> <li> Java <br> </li>  <li> C/C++ <br> </li>  <li> CP/CHR <br> </li>  <li> Prolog <br></li> <li> Haskell <br></li>  <li> ASP.net <br></li> <li> C# <br></li>  <li>Visual studio </li></ul>";
+var x = document.getElementById("C");
+if(x.innerHTML == "<ul> <li> Java <br> </li>  <li> C/C++ <br> </li>  <li> CP/CHR <br> </li>  <li> Prolog <br></li> <li> Haskell <br></li>  <li> ASP.net <br></li> <li> C# <br></li>  <li>Visual studio </li></ul>"){
+x.innerHTML = "";
+}else{
+x.innerHTML="<ul> <li> Java <br> </li>  <li> C/C++ <br> </li>  <li> CP/CHR <br> </li>  <li> Prolog <br></li> <li> Haskell <br></li>  <li> ASP.net <br></li> <li> C# <br></li>  <li>Visual studio </li></ul>";
+}
+}
+
+function myFunction4() {
+window.location = "https://www.facebook.com/heba.aamer.355";
 }
