94361dfd9a9fb1bd28e952ed5d6c77eeaa60fe27
Shaban <mohamed19936@gmail.com>
2014-03-28 17:01:05 +0200

    Issue #114 added new constructor to postRequest class

--- Entangle/entangle-android/src/com/megasoft/requests/GetRequest.java
+++ Entangle/entangle-android/src/com/megasoft/requests/GetRequest.java
@@ -4,8 +4,8 @@ import org.apache.http.client.methods.HttpGet;
 
 public class GetRequest extends HttpRequest {
 	HttpGet httpGet;
-	
-	public GetRequest(String uri){
+
+	public GetRequest(String uri) {
 		httpGet = new HttpGet(uri);
 		super.setMethod(httpGet);
 	}
