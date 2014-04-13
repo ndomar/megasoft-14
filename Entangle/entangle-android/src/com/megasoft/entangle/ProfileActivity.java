3bfda0b381c3b21c36856b36ec76c4a25c716b83
Alaa Maher <almgohar@gmail.com>
2014-04-14 00:48:13 +0200

    Issue #147 - Modify the frontend code style.

--- Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -1,5 +1,11 @@
 package com.megasoft.entangle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,6 +14,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +25,7 @@ public class ProfileActivity extends Activity {
 	private TextView descr;
 	private TextView balance;
 	private TextView birthdate;
+	private TextView transaction;
 	private ImageView verifiedView;
 	final Activity self = this;
     LinearLayout layoutContainer;
@@ -26,12 +34,9 @@ public class ProfileActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
-			
-				try {
-
+			try {
 					viewProfile();
 				} catch (JSONException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			
@@ -39,60 +44,60 @@ public class ProfileActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
 	public void viewProfile() throws JSONException {
+		layoutContainer = (LinearLayout) this.findViewById(R.id.layout_container);
 		name = (TextView) findViewById(R.id.nameView);
 		descr = (TextView) findViewById(R.id.descriptionView);
 		balance = (TextView) findViewById(R.id.balanceView);
 		birthdate = (TextView) findViewById(R.id.birthdateView);
 		verifiedView = (ImageView) findViewById(R.id.verifiedView);
+		
 		Intent intent = getIntent();
-		layoutContainer = (LinearLayout) this.findViewById(R.id.layout_container);
 		int tangleId = intent.getIntExtra("tangle id", -1);
 		int userId = intent.getIntExtra("user id", -1);
-
-		GetRequest request = new GetRequest("http://entangle2.apiary-mock.com/tangle/" + tangleId + "/user/" + userId + "/profile") {
-			
-			protected void onPostExecute(String response) {  // On Post execute means after the execution of the request ( the callback )
-                     try {
+		
+		String link = "http://entangle2.apiary-mock.com/tangle/" + tangleId + "/user/" + userId + "/profile";
+		getInformation(link);
+	}
+	
+	public Hashtable<String, Object> getInformation(String link) {
+		 final Hashtable<String, Object> userInformation = new Hashtable<String, Object>();
+		GetRequest request = new GetRequest(link) {
+			protected void onPostExecute(String response) {  
+				try {
 						JSONObject jSon = new JSONObject(response);
-						//boolean loggedIn = jSon.getBoolean("loggedIn");
-						
-						JSONObject info =  jSon.getJSONObject("information");
-						name.setText(info.getString("name"));
-						descr.setText("Description: " + info.getString("Description"));
-						balance.setText("Credit: " + info.getString("balance") + " points");
-						birthdate.setText("Birthdate: " + info.getString("birthdate"));
-						boolean verified = info.getBoolean("verified");
-						if (verified) {
-							verifiedView.setVisibility(1);
-						} else {
-							verifiedView.setVisibility(0);
-						}
+						JSONObject information =  jSon.getJSONObject("information");
 						JSONArray array = jSon.getJSONArray("transactions");
+
+						userInformation.put("loggedIn", jSon.getBoolean("loggedIn"));
+						userInformation.put("name", information.getString("name"));
+						userInformation.put("description", information.getString("Description"));
+						userInformation.put("credit", information.getString("balance"));
+						userInformation.put("birthdate", information.getString("birthdate"));
+						userInformation.put("verified",information.getBoolean("verified"));
+						
 						for(int i = 0; i < array.length(); i++) { 
 							JSONObject object = array.getJSONObject(i);
-							TextView transaction = new TextView(self);
 							String requester = object.getString("requesterName");
 							String request = object.getString("requestDescription");
 							String amount = object.getString("amount");
+							transaction = new TextView(self);
 							transaction.setText("Requester: " + requester 
 									+ '\n' + "Request: " + request
 									+ '\n' + "Amount: " + amount);
 							layoutContainer.addView(transaction);
-						}
-                     } catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-                  }
-		};
-		request.execute();
+							}
+						} catch (JSONException e) {
+								e.printStackTrace();
+								}
+				}
+			};
+			request.execute();
+			return userInformation;	
 	}
-	
 
 }
