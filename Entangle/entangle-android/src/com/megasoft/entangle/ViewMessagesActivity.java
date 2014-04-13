7480384337d9e1b81cb508edc12bf1e24cfb7d19
naderdavid <emailandmail@yahoo.com>
2014-04-13 17:45:58 +0200

    Issue  #167 - adding skeleton for viewing all messages

--- Entangle/entangle-android/src/com/megasoft/entangle/ViewMessagesActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ViewMessagesActivity.java
@@ -21,7 +21,6 @@ public class ViewMessagesActivity extends Activity {
 		
 	}
 	
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.view_messages, menu);
