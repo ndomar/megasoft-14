4b6bb2ba4756ed76f329001d2155ea12ca578023
heba-aamer <heba93aamer@gmail.com>
2014-04-13 17:07:17 +0200

    Issue #109 - Removing photos

--- Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
@@ -1,10 +1,8 @@
 package com.megasoft.entangle;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import com.megasoft.entangle.TangleProfilePage;
 
 public class MainActivity extends Activity {
 
@@ -12,7 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 	}
 
 	@Override
