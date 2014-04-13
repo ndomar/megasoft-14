143dc945d88a55677c2159b9853878dfcfca59fb
silvermist <sue.fairy@live.com>
2014-04-13 23:43:37 +0200

    Issue #128 - adding checks on Empty input

--- Entangle/entangle-android/src/com/megasoft/entangle/RequestActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/RequestActivity.java
@@ -1,16 +1,16 @@
 package com.megasoft.entangle;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.megasoft.requests.PostRequest;
 
 
@@ -30,39 +30,103 @@ public class RequestActivity extends Activity{
      */
  	
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		Intent previousIntent = getIntent();
 		final int tangleID = previousIntent.getIntExtra("tangleID" , 0);
 		final String sessionId = previousIntent.getStringExtra("sessionId");
-		
-		EditText description   = (EditText) findViewById(R.id.editText2);
-		 EditText requestedPrice   = (EditText) findViewById(R.id.editText5);
-		 EditText date   = (EditText) findViewById(R.id.editText1);
-		 EditText deadLine   = (EditText) findViewById(R.id.editText4);
-	    EditText tags   = (EditText) findViewById(R.id.editText3);
-	    Button Post    = (Button) findViewById(R.id.button1);
-	  
-		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_request);
-		
+		final EditText description = (EditText) findViewById(R.id.description);
+		final EditText requestedPrice = (EditText) findViewById(R.id.price);
+		final EditText date = (EditText) findViewById(R.id.date);
+		final EditText deadLine = (EditText) findViewById(R.id.deadLine);
+	    final EditText tags = (EditText) findViewById(R.id.tags);
+	    final Button Post = (Button) findViewById(R.id.post);
 		final JSONObject json = new JSONObject();
-        try {
-        	
-            json.put("description" , description );
-            json.put("requestedPrice" , requestedPrice );
-            json.put("date" , date );
-            json.put("deadLine" , deadLine );
-            json.put("tags", tags);
-            
-           
-            
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+		
+		description.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			public void onFocusChange(View v, boolean hasFocus) {
+			    if(!hasFocus){
+			    	if(isEmpty(description)) {
+					Post.setEnabled(false);
+				}else {
+					Post.setEnabled(true);
+					}
+				}
+			   }
+			});
+        
+		tags.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			public void onFocusChange(View v, boolean hasFocus) {
+			    if(!hasFocus){
+			    	if(isEmpty(tags)) {
+					Post.setEnabled(false);
+				}else {
+					Post.setEnabled(true);
+					}
+				}
+			   }
+			});
+        
+		date.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	 
+			
+			public void onFocusChange(View v, boolean hasFocus) {
+			    if(!hasFocus){
+			    	if(isEmpty(date)) {
+					Post.setEnabled(false);
+				}else {
+					Post.setEnabled(true);
+					}
+				}
+			   }
+			});
+      
+		deadLine.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	 
+			
+			public void onFocusChange(View v, boolean hasFocus) {
+			    if(!hasFocus){
+			    	if(isEmpty(deadLine)) {
+					Post.setEnabled(false);
+				}else {
+					Post.setEnabled(true);
+					}
+				}
+			   }
+			});
+        requestedPrice.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	 
+			
+			public void onFocusChange(View v, boolean hasFocus) {
+			    if(!hasFocus){
+			    	if(isEmpty(requestedPrice)) {
+					Post.setEnabled(false);
+				}else {
+					Post.setEnabled(true);
+					}
+				}
+			   }
+			});
+		
 
         Post.setOnClickListener(new View.OnClickListener() {
 			
 			
 			public void onClick(View arg0) {
+				  try {
+			        	
+			            json.put("description" , description.getText().toString());
+			            json.put("requestedPrice" , requestedPrice.getText().toString());
+			            json.put("date" , date.getText().toString());
+			            json.put("deadLine" , deadLine.getText().toString());
+			            json.put("tags", tags.getText().toString());
+			            
+			           } catch (JSONException e) {
+			            e.printStackTrace();
+			           }
+				
 				
 				 PostRequest request = new PostRequest("http://entangle2.io/tangle/" + tangleID + "/request"){
 			            protected void onPostExecute(String response) {  
@@ -73,14 +137,23 @@ public class RequestActivity extends Activity{
 			                  }
 			             }
 			        };
-			        request.setBody(json); 
+			       request.setBody(json); 
 					request.addHeader("X-SESSION-ID", sessionId);
 			        request.execute();
 			      
 			}
-		});
+		}); 
       
 	}
+	private boolean isEmpty(EditText editText){
+		if( editText.getText().toString().length() == 0 ){
+		    editText.setError("Enter something I'm giving up on you");
+		    return true;
+		}
+		editText.setError(null);
+		return false;
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
