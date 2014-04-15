fcc37aaa25575aaa8f08480a5001e20e650207c8
Alaa Maher <almgohar@gmail.com>
2014-04-15 15:19:23 +0200

    Issue #147 - edit frontend to view own profile

--- Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -3,26 +3,31 @@ package com.megasoft.entangle;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.megasoft.config.Config;
 import com.megasoft.requests.GetRequest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
 	private TextView name;
 	private TextView descr;
 	private TextView balance;
 	private TextView birthdate;
+	private TextView edit;
 	private ImageView verifiedView;
-	final Activity self = this;
     LinearLayout layoutContainer;
-
+	SharedPreferences settings;
+	final Activity self = this;
+	int loggedInId;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -44,30 +49,29 @@ public class ProfileActivity extends Activity {
 	}
 	
 	public void viewProfile() throws JSONException {
+		edit = (TextView) findViewById(R.id.EditProfile);
 		name = (TextView) findViewById(R.id.nameView);
 		balance = (TextView) findViewById(R.id.balanceView);
 		descr = (TextView) findViewById(R.id.descriptionView);
 		birthdate = (TextView) findViewById(R.id.birthdateView);
 		verifiedView = (ImageView) findViewById(R.id.verifiedView);
 		layoutContainer = (LinearLayout) this.findViewById(R.id.layout_container);
-		
+		this.settings = getSharedPreferences(Config.SETTING, 0);
+		this.loggedInId = settings.getInt(Config.USER_ID, -1);
 		Intent intent = getIntent();
 		int tangleId = intent.getIntExtra("tangle id", -1);
 		int userId = intent.getIntExtra("user id", -1);
-		
-		String link = "http://entangle2.apiary-mock.com/tangle/" 
-		+ tangleId + "/user/" + userId + "/profile";
-		setInformation(link);
+		setInformation(userId, tangleId);
 	}
 	
-	public void setInformation(String link) {
+	public void setInformation(final int userId, int tangleId) {
+		String link = "http://entangle2.apiary-mock.com/tangle/" 
+	+ tangleId + "/user/" + userId + "/profile";
 		GetRequest request = new GetRequest(link) {
 			protected void onPostExecute(String response) {
-				Log.e("7mada", response);
 				JSONObject jSon;
 				try {
 					jSon = new JSONObject(response);
-			//		boolean loggedIn = jSon.getBoolean("loggedIn");
 					JSONArray transactions = jSon.getJSONArray("transactions");
 					JSONObject information =  jSon.getJSONObject("information");
 					setTransactions(transactions);
@@ -76,12 +80,18 @@ public class ProfileActivity extends Activity {
 					balance.setText("Credit: " + information.getString("balance") + " points");
 					birthdate.setText("Birthdate: " + information.getString("birthdate"));
 					boolean verified = information.getBoolean("verified");
-					
 					if (verified) {
-							verifiedView.setVisibility(1);
-							} else {
-								verifiedView.setVisibility(0);
-								}
+							verifiedView.setVisibility(View.VISIBLE);
+							} 
+					if(loggedInId == userId) {
+						edit.setVisibility(View.VISIBLE);
+						edit.setOnClickListener(new View.OnClickListener() {
+							@Override
+				            public void onClick(View v) {
+				            	goToEditProfile();
+				            	}
+				            });
+						}
 					} catch (JSONException e) {
 						e.printStackTrace();
 						}
@@ -108,4 +118,10 @@ public class ProfileActivity extends Activity {
 					}
 			}
 		}
+	
+	public void goToEditProfile() {
+		Intent editProfile = new Intent(this, EditProfileActivity.class);
+		editProfile.putExtra("user id", loggedInId);
+		startActivity(editProfile);
+	}
 	}
