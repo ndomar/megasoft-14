3254caccdd4063f5935aaa26726027685228011b
Alaa Maher <almgohar@gmail.com>
2014-04-15 21:51:08 +0200

    Issue #147 - finalize the frontend.

--- Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -1,14 +1,20 @@
 package com.megasoft.entangle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.megasoft.config.Config;
 import com.megasoft.requests.GetRequest;
+import com.megasoft.requests.ImageRequest;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -17,12 +23,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
+	private TextView edit;
 	private TextView name;
 	private TextView descr;
 	private TextView balance;
 	private TextView birthdate;
-	private TextView edit;
 	private ImageView verifiedView;
+	private ImageView profilePictureView;
     LinearLayout layoutContainer;
 	SharedPreferences settings;
 	final Activity self = this;
@@ -55,16 +62,17 @@ public class ProfileActivity extends Activity {
 		descr = (TextView) findViewById(R.id.descriptionView);
 		birthdate = (TextView) findViewById(R.id.birthdateView);
 		verifiedView = (ImageView) findViewById(R.id.verifiedView);
-		layoutContainer = (LinearLayout) this.findViewById(R.id.layout_container);
+		profilePictureView = (ImageView)findViewById(R.id.profileImage);
+		layoutContainer = (LinearLayout) this.findViewById(R.id.transactions_layout);
 		this.settings = getSharedPreferences(Config.SETTING, 0);
 		this.loggedInId = settings.getInt(Config.USER_ID, -1);
 		Intent intent = getIntent();
 		int tangleId = intent.getIntExtra("tangle id", -1);
 		int userId = intent.getIntExtra("user id", -1);
-		setInformation(userId, tangleId);
+		viewInformation(userId, tangleId);
 	}
 	
-	public void setInformation(final int userId, int tangleId) {
+	public void viewInformation(final int userId, int tangleId) {
 		String link = "http://entangle2.apiary-mock.com/tangle/" 
 	+ tangleId + "/user/" + userId + "/profile";
 		GetRequest request = new GetRequest(link) {
@@ -74,11 +82,12 @@ public class ProfileActivity extends Activity {
 					jSon = new JSONObject(response);
 					JSONArray transactions = jSon.getJSONArray("transactions");
 					JSONObject information =  jSon.getJSONObject("information");
-					setTransactions(transactions);
+					viewTransactions(transactions);
 					name.setText(information.getString("name"));
 					descr.setText("Description: " + information.getString("Description"));
 					balance.setText("Credit: " + information.getString("balance") + " points");
 					birthdate.setText("Birthdate: " + information.getString("birthdate"));
+					viewProfilePicture(information.getString("picture URL"));
 					boolean verified = information.getBoolean("verified");
 					if (verified) {
 							verifiedView.setVisibility(View.VISIBLE);
@@ -100,7 +109,11 @@ public class ProfileActivity extends Activity {
 			request.execute();
 			}
 	
-	public void setTransactions(JSONArray transactions) {
+	public void viewTransactions(JSONArray transactions) {
+		TextView title = new TextView(self);
+		title.setText("Transactions: ");
+		title.setTextSize(20);
+		layoutContainer.addView(title);
 		for(int i = 0; i < transactions.length(); i++) {
 			JSONObject object;
 			try {
@@ -118,6 +131,11 @@ public class ProfileActivity extends Activity {
 					}
 			}
 		}
+	
+	public void viewProfilePicture(String imageURL) {
+            ImageRequest image = new ImageRequest(profilePictureView);
+            image.execute(imageURL);
+	}
 	
 	public void goToEditProfile() {
 		Intent editProfile = new Intent(this, EditProfileActivity.class);
