0aab0b6a21c972ff8871bfd9ba54baaf164e7b82
mennatarek <mennatallah.dawood@gmail.com>
2014-04-16 01:16:04 +0200

    Front End Layout Modification

---
+++ Entangle/entangle-android/src/com/megasoft/entangle/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.megasoft.entangle;
+
+import com.megasoft.entangle.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditProfileActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_profile);
+	}
+
+}
