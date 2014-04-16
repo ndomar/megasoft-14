fed13053f7847de37c90d57b3a078a03d4bf27e7
sak93 <sarakhodeir@gmail.com>
2014-04-16 11:28:31 +0300

    Issue #131 - recieve offers in ViewOffers activity

--- Entangle/entangle-android/src/com/megasoft/entangle/ViewOffers.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ViewOffers.java
@@ -1,5 +1,20 @@
 package com.megasoft.entangle;
 
-public class ViewOffers {
 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewOffers extends Activity {
+	public String[][] offerDetails; 
+
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_offers);
+        Bundle bundle = getIntent().getExtras();
+        offerDetails = (String[][])bundle.getSerializable("offers");	
+	}
+
+	
 }
+
+
