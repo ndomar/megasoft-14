66fda9fbb808726af34d6c6c63e733d2af6f541b
farghalico <mohamed@farghal.com>
2014-04-16 12:47:56 +0200

    Added rounded image view and enhanced the structure of sidebar #189

--- Entangle/entangle-android/src/com/megasoft/entangle/HomeActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/HomeActivity.java
@@ -1,28 +1,11 @@
 package com.megasoft.entangle;
 
-
-import java.util.Locale;
-
-import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class HomeActivity extends FragmentActivity {
@@ -36,7 +19,7 @@ public class HomeActivity extends FragmentActivity {
 
 		String[] listTitles  	= getResources().getStringArray(R.array.sidebar_list);
 		DrawerLayout drawer 	= (DrawerLayout) findViewById(R.id.drawer_layout);
-		ListView drawerList 	= (ListView) findViewById(R.id.left_drawer);
+		ListView drawerList 	= (ListView) findViewById(R.id.tangleList);
 		
 		drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.sidebar_list_item, R.id.textView1, listTitles));
 		
