8009bec202e7845af2980477df895e476163237c
Mansour <mansoursaid001@hotmail.com>
2014-04-16 03:27:34 +0200

    Issue #110 - Semi working back-end without doc

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/TangleController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/TangleController.php
@@ -230,4 +230,34 @@ class TangleController extends Controller {
         return $ret;
     }
 
+    public function createTangleAction(Request $request) {
+        $json = $request->getContent();
+        $json_array = json_decode($json, true);
+        $tangleName = $json_array['tangleName'];
+        $tangleIcon = $json_array['tangleIcon'];
+
+        $Tangle = new Tangle();
+        $Tangle->setName($tangleName);
+        $Tangle->setIcon($tangleIcon);
+
+        $this->getDoctrine()->getManager()->persist($Tangle);
+        $this->getDoctrine()->getManager()->flush();
+
+        $response = new Response();
+        $response->setStatusCode(201);
+        return $response;
+    }
+
+    public function checkAvailabilityAction($tangleName) {
+        $doctrine = $this->getDoctrine();
+        $repo = $doctrine->getRepository('MegasoftEntangleBundle:Tangle');
+        $tangle = $repo->findOneBy(array('name' => $tangleName));
+
+        if ($tangle == null) {
+            return new Response('Not Found', 404);
+        } else {
+            return new Response('Found', 302);
+        }
+    }
+
 }
