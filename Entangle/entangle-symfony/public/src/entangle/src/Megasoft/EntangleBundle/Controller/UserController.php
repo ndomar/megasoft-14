fa57ca26b00a7c9ff612a9e5fa6f451203b19d24
Alaa Maher <almgohar@gmail.com>
2014-04-16 13:11:18 +0200

    Issue #147 - Edit the backend code style

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
@@ -17,73 +17,62 @@ use Megasoft\EntangleBundle\Entity\Session;
  */
 class ProfileController {
     
-    //gets all required info to view a user profile
-    //returns a jSonResponse
-        public function profile(Request $request, $userId, $tangleId) {
+    public function profile (Request $request, $userId, $tangleId) {
         $sessionId = $request->headers->get('X-SESSION-ID');
         $doctrine = $this->getDoctrine();
         $userTable = $doctrine->getRepository('MegasoftEntangleBundle:User');
         $sessionTable = $doctrine->getRepository('MegasoftEntangleBundle:Session');
-        $session = $sessionTable->findOneBy(array('id'=>$sessionId));
+        $session = $sessionTable->findOneBy(array('sessionId'=>$sessionId));
         $user = $userTable->findOneBy(array('id'=> $userId));
         $offers = $user->getOffers();
         $info = $this->getUserInfo($user, $tangleId);
         $transactions = $this->getTransactions($offers, $tangleId);
-        $loggedInUser = $session->getUserId(); //Id of the currently logged in user
-        
-        if($userId == $loggedInUser) {
-            $login = true;
-        } else {
-            $login = false;
-        }
-        
-            $response = new JsonResponse();
-            $response->setData(array('loggedIn'=>$login, 'information'=> $info, 
-                'transactions'=>$transactions));
-            $response->setStatusCode(200);
-            return $response;
-        }
-   
-    //gets the transactions of this user in a certain tangle
-    //returns and array of arrays
-    public function getTransactions($offers, $tangleId) {
+        $response = new JsonResponse();
+        $response->setData(array('information'=> $info,
+            'transactions'=>$transactions));
+        $response->setStatusCode(200);
+        return $response;       
+    }
+    
+    public function getTransactions ($offers, $tangleId) {
         $transactions = array();
-        for($i = 0; i < count($offers); $i++) {
+        for ($i = 0; i < count($offers); $i++) {
             $offer = $offers[i];
-            if($offer == null) {
+            if ($offer == null) {
                 continue;
             }
-            if(($offer->getRequest()->getTangleId() == $tangleId) 
-                    && ($offer->getTransaction() != null)) { 
+            if (($offer->getRequest()->getTangleId() == $tangleId)
+                    && ($offer->getTransaction() != null)) {
                 $requesterName =$offer->getRequest()->getUser()->getName();
                 $requestDescription = $offer->getRequest()->getDescription();
                 $amount = $offer->getTransaction()->getRequestedPrice();
-                $transactions[] = array('requesterName'=> $requesterName, 
-                    'requestDescription'=>$requestDescription,'amount'=>$amount);
-            }
-            }
-            return $transactions;
+                $transactions[] = array('requesterName'=> $requesterName,
+                    'requestDescription'=>$requestDescription,
+                    'amount'=>$amount);
+            }           
+        }
+        return $transactions;    
     }
     
-    //gets the basic information of the required user
-    //returns an array of these information
-    public function getUserInfo($user, $tangleId){
-        if($user == null){
+    public function getUserInfo ($user, $tangleId) {
+        if ($user == null) {
             return new Response('Bad Request',400);
         }
         $doctrine = $this->getDoctrine();
         $userId = $user->getId();
-        $userTangleTable = $doctrine->getRepository('MegasoftEntangleBundle:UserTangle');
-        $userTangle = $userTangleTable->findOneBy(array('userId'=>$userId,'tangleId'=>$tangleId));
-        
+        $userTangleTable = $doctrine->
+                getRepository ('MegasoftEntangleBundle:UserTangle');
+        $userTangle = $userTangleTable->
+                findOneBy(array ('userId'=>$userId,'tangleId'=>$tangleId)); 
         $name = $user->getName();
         $description = $user->getUserBio();
         $credit = $userTangle->getCredit();
         $photo = $user->getPhoto();
         $birthdate = $user->getBirthDate();
         $verfied = $user->getVerified();
-        $info = array('name'=>$name, 'description'=>$description, 'credit'=>$credit,
-                'photo'=>$photo, 'birthdate'=>$birthdate, 'verified' => $verfied);
+        $info = array ('name'=>$name , 'description'=>$description , 
+            'credit'=>$credit, 'photo'=>$photo , 'birthdate'=>$birthdate , 
+            'verified' => $verfied);
         return $info;
     }
     
