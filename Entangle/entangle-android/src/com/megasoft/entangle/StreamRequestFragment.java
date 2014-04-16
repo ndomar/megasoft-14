8c8188d7d435533c98448a59d18cb53ee30ccb4c
heba-aamer <heba93aamer@gmail.com>
2014-04-16 07:04:22 +0200

    Issue #109 - Adding methods to Request Fragment class

--- Entangle/entangle-android/src/com/megasoft/entangle/StreamRequestFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/StreamRequestFragment.java
@@ -4,60 +4,51 @@ import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 @SuppressLint("NewApi")
 public class StreamRequestFragment extends Fragment {
+	// public static int counter = 0;
+	// private int num;
 	private int requesterId;
 	private int requestId;
+	private String requestString;
+	private String requesterString;
 	Button request;
 	Button requester;
 
+	public static StreamRequestFragment createInstance(int requestId,
+			int requesterId, String requestString, String requesterString) {
+		StreamRequestFragment fragment = new StreamRequestFragment();
+		fragment.setRequestId(requestId);
+		fragment.setRequesterId(requesterId);
+		fragment.setRequestButtonText(requestString);
+		fragment.setRequesterButtonText(requesterString);
+		return fragment;
+	}
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstancState) {
-		return inflater.inflate(R.layout.activity_stream_request_fragment,
+		View view = inflater.inflate(R.layout.stream_request_fragment,
 				container, false);
+		request = (Button) view.findViewById(R.id.requestButton);
+		// setRequestRedirection();
+		requester = (Button) view.findViewById(R.id.requesterButton);
+		// setRequesterRedirection();
+		return view;
 	}
 
-	public void onStart() {
-		requester = (Button) getActivity().findViewById(R.id.requesterButton);
-		requester.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getActivity().getBaseContext(),
-						Profile.class);
-				intent.putExtra("tangleId",
-						((TangleProfilePage) getActivity()).getTangleId());
-				intent.putExtra("tangleName",
-						((TangleProfilePage) getActivity()).getTangleName());
-				intent.putExtra("sessionId",
-						((TangleProfilePage) getActivity()).getSessionId());
-				intent.putExtra("requesterId", getRequesterId());
-				startActivity(intent);
-			}
-		});
-		request = (Button) getActivity().findViewById(R.id.requestButton);
-		request.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getActivity().getBaseContext(),
-						RequestPage.class);
-				intent.putExtra("tangleId",
-						((TangleProfilePage) getActivity()).getTangleId());
-				intent.putExtra("tangleName",
-						((TangleProfilePage) getActivity()).getTangleName());
-				intent.putExtra("sessionId",
-						((TangleProfilePage) getActivity()).getSessionId());
-				intent.putExtra("requestId", getRequestId());
-				startActivity(intent);
-			}
-		});
-	}
+	// @Override
+	// public void onSaveInstanceState(Bundle outState) {
+	// Log.d("Method", "onSave");
+	// }
 
 	private int getRequestId() {
 		return requestId;
@@ -67,20 +58,33 @@ public class StreamRequestFragment extends Fragment {
 		return requesterId;
 	}
 
-	public void setRequesterId(int id) {
+	private String getRequestString() {
+		return requestString;
+	}
+
+	private String getRequesterString() {
+		return requesterString;
+	}
+
+	private void setRequesterId(int id) {
 		requesterId = id;
 	}
 
-	public void setRequestId(int id) {
+	private void setRequestId(int id) {
 		requestId = id;
 	}
 
-	public void setRequesterButtonText(String text) {
-		requester.setText(text);
+	private void setRequesterButtonText(String text) {
+		// requester = (Button)
+		// getActivity().findViewById(R.id.requesterButton);
+		// requester.setText(text);
+		requesterString = text;
 	}
 
-	public void setRequestButtonText(String text) {
-		request.setText(text);
+	private void setRequestButtonText(String text) {
+		// request = (Button) getActivity().findViewById(R.id.requestButton);
+		// request.setText(text);
+		requestString = text;
 	}
 
 }
