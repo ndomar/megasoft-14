b1a1a0eae93c417621894a2746093055699be1a3
heba-aamer <heba93aamer@gmail.com>
2014-04-17 17:38:44 +0200

    Issue #109 - Adding suggestions in FilteringFragment

--- Entangle/entangle-android/src/com/megasoft/entangle/FilteringFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/FilteringFragment.java
@@ -1,16 +1,36 @@
 package com.megasoft.entangle;
 
+import java.util.HashMap;
+
 import android.annotation.SuppressLint;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 @SuppressLint("NewApi")
 public class FilteringFragment extends DialogFragment {
+
+	private String rootResource = "http://entangle2.apiary.io/";
+
+	private HashMap<String, Integer> userToId = new HashMap<String, Integer>();
+
+	private HashMap<String, Integer> tagToId = new HashMap<String, Integer>();
+
+	public static FilteringFragment createInstance(
+			HashMap<String, Integer> tagToId, HashMap<String, Integer> userToId) {
+		FilteringFragment fragment = new FilteringFragment();
+		fragment.tagToId = tagToId;
+		fragment.userToId = userToId;
+		return fragment;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,7 +40,6 @@ public class FilteringFragment extends DialogFragment {
 				false);
 		setTagSuggestions(view);
 		setUserSuggestions(view);
-		setButtonsActions(view);
 		return view;
 	}
 
@@ -34,13 +53,12 @@ public class FilteringFragment extends DialogFragment {
 
 	private void setUserSuggestions(View view) {
 		AutoCompleteTextView user = (AutoCompleteTextView) view
-				.findViewById(R.id.tagValue);
+				.findViewById(R.id.userValue);
 		user.setAdapter(new ArrayAdapter<String>(getActivity(),
 				android.R.layout.simple_list_item_1,
 				((TangleProfilePage) getActivity()).getUsersSuggestions()));
 	}
 
-	private void setButtonsActions(View view) {
+	
 
-	}
 }
