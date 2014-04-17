a7a5ffa5c7ac23c460bd1b2a0fd728be5493d654
Eslamunto <eslam.shala@gmail.com>
2014-04-17 15:29:49 +0200

    Issue #161 - Add feature for profile picture uploading

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/DefaultController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Megasoft\EntangleBundle\Controller;
 use Megasoft\EntangleBundle\Entity\InvitationMessage;
 use Megasoft\EntangleBundle\Entity\PendingInvitation;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Exception\LogicException;
 use Megasoft\EntangleBundle\Entity\User;
@@ -14,6 +16,14 @@ class DefaultController extends Controller {
 
     public function indexAction($name) {
         return $this->render('MegasoftEntangleBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function getImageURL($originalName) {
+        $outputFilePath = 'http://127.0.0.1:8000/public/src/entangle/web/bundles/megasoftentangle/images/profilePictures/' . $originalName;
+        file_put_contents($originalName, $outputFilePath);
+
+
+        return 'http://127.0.0.1:8000/entangle/web/bundles/megasoftentangle/images/profilePictures/' . $originalName ;
     }
 
     public function registerAction(\Symfony\Component\HttpFoundation\Request $request)  {
@@ -38,6 +48,45 @@ class DefaultController extends Controller {
             $user->setVerified($verified);
             $userEmail->setEmail($email);
 
+            $image = $request->files->get('img');
+            $status = 'succes';
+            $uploadedURL = '';
+            $message='';
+            if(($image instanceof UploadedFile)&&($image->getError() == '0')) {
+                if($image->getSize() < 4194304) { //if image size is less that 4MB
+                    $originalName = $image->getClientOriginalName();
+                    $nameArray = explode('.', $originalName);
+                    $fileType = $nameArray[sizeof($nameArray) - 1];
+                    $validFileTypes = array('jpg', 'jpeg' , 'bmp' ,
+                    'png');
+
+                    if(in_array(strtolower($fileType), $validFileTypes)) {
+                        try{
+                            $uploadedURL = $this->getImageUrl($originalName);
+                            $user->setPhoto($uploadedURL);
+                        }
+                        catch(Exception $e) {
+                            return new Response('Internal Server Error', 500);
+                        }
+                    }
+                    else {
+                        $status = 'failed';
+                        $message = 'Invalid File Type';
+                    }
+                }
+                else {
+                    $status = 'failed';
+                    $message = 'Size limit exceeded';
+                }
+            }
+            else {
+                $status = 'failed';
+                $message = 'File Error';
+            }
+
+
+
+
 
             $entityManager = $this->getDoctrine()->getEntityManager();
             $entityManager->persist($user);
@@ -49,68 +98,69 @@ class DefaultController extends Controller {
         return $this->render('MegasoftEntangleBundle:Default:register.html.twig');
     }
 
+
 //    public function testAction(){
 //        $doctrineManger = $this->getDoctrine()->getManager();
-//        
+//
 //        $user = new User();
-//        
+//
 //        $userEmail = new UserEmail();
 //        $userEmail->setEmail("test@megasoft.com");
-//        
+//
 //        $user->setName("Mohamed");
 //        $user->setPassword(md5("test"));
-//        $user->setVerified(0); 
+//        $user->setVerified(0);
 //        $user->addEmail($userEmail);
-//        
+//
 //        $userTangle = new UserTangle();
 //        $userTangle->setCredit(100);
 //        $userTangle->setTangleOwner(0);
-//        
+//
 //        $tangle = new Tangle();
 //        $tangle->setName("Prince el koon");
 //        $tangle->addUserTangle($userTangle);
-//        
+//
 //        $user->addUserTangle($userTangle);
-//        
+//
 //        $request = new Request();
 //        $request->setTangle($tangle);
 //        $request->setStatus(1);
 //        $request->setDescription("test");
 //        $request->setDate(new \DateTime("NOW"));
 //        $request->setUser($user);
-//        
+//
 //        $tag1 = new Tag();
 //        $tag1->setName("prince");
 //        $request->addTag($tag1);
-//        
+//
 //        $tag2 = new Tag();
 //        $tag2->setName("prince2");
 //        //$tag2->addRequest($request); // Not Working yet
-//        
+//
 //        $offer = new Offer();
 //        $offer->setRequestedPrice(20);
 //        $offer->setDate(new \DateTime());
 //        $offer->setDescription("lkjadfhsf");
 //        $offer->setStatus(0);
-//        
+//
 //        $request->addOffer($offer);
-//        
+//
 //        $offer2 = new Offer();
 //        $offer2->setRequestedPrice(20);
 //        $offer2->setDate(new DateTime());
 //        $offer2->setDescription("lkjadfhsf");
 //        $offer2->setStatus(0);
 //        $offer2->setRequest($request);
-//        
+//
 //        $transaction = new Transaction();
 //        $transaction->setDate(new DateTime("NOW"));
 //        $transaction->setOffer($offer);
-//        
+//
 //        $message = new Message();
 //        $message->setDate(new DateTime("NOW"));
 //        $message->setOffer($offer);
 //        $message->setSender($user);
-//        
+//
 //        $messageNotification = new NewMessageNotification();
 //        $messageNotification->setCreated(new Datetime("NOW"));
 //        $messageNotification->setSeen(0);
@@ -136,16 +186,16 @@ class DefaultController extends Controller {
 //        $session->setExpired(0);
 //        $session->setUser($user);
 //        $session->setSessionId("sdakjasdfhlkajsdfhasdf");
-          
+
 //        $invitationMessage = new InvitationMessage();
 //        $invitationMessage->setBody("test");
-          
+
 //        $pendingInvitation = new PendingInvitation();
 //        $pendingInvitation->setInvitee($doctrineManger->getReference('MegasoftEntangleBundle:User',1));
 //        $pendingInvitation->setInviter($doctrineManger->getReference('MegasoftEntangleBundle:User',2));
 //        $pendingInvitation->setTangle($doctrineManger->getReference('MegasoftEntangleBundle:Tangle',1));
 //        $pendingInvitation->setMessage($doctrineManger->getReference('MegasoftEntangleBundle:InvitationMessage',2));
-          
+
 //        $doctrineManger->persist($user);
 //        $doctrineManger->persist($userEmail);
 //        $doctrineManger->persist($tangle);
