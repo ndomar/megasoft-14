be32b008fc3685ad1e27205a7c6ecc46fba9ab6c
naderdavid <emailandmail@yahoo.com>
2014-04-17 19:52:02 +0200

    Issue #167 - getting all messages of an offer

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/MessageController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/MessageController.php
@@ -24,6 +24,35 @@ class MessageController extends Controller
             return new Response('Bad Request', 400);
         }
     }
+    /**
+     * getting all messages on that offer
+     * @param \Megasoft\EntangleBundle\Controller\Request $request
+     * @param  integer $offerId
+     * @return \Megasoft\EntangleBundle\Controller\Response $response
+     * @author NaderNessem
+     */
+    public function getMessages(Request $request,$offerId){
+        $verify = verify($request,$offerId);
+        if(\is_null($verify)){
+            return verify;
+        }
+        $doctrine = $this->getDoctrine();
+        $messagesRepo = $doctrine->getRepository('MegasoftEntangleBundle:Message');
+        $messages = $messagesRepo->findOneBy(array('offerId' => $offerId));
+        $jsonArray = array();
+        foreach($messages as $message) {
+            $senderId= $message->getSenderId();
+            $senderRepo = $doctrine->getRepository('MegasoftEntangleBundle:User');
+            $sender = $senderRepo->findOneBy(array('id' => $senderId));
+            $jsonArray ['senderName'] =$sender->getName();
+            $jsonArray ['body'] = $message->getBody();
+            $jsonArray ['date'] = $message->getDate();
+        }
+        $response = json_encode($jsonArray);
+        return $response;
+        
+    }
+    
     
 }
 
