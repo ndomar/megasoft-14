3d8e63baa3cca309d2444840871b2734e448d157
Alaa Maher <almgohar@gmail.com>
2014-04-18 21:27:47 +0200

    Issue #147 - Adding the documentation to the frontend code

--- Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -1,52 +1,100 @@
 package com.megasoft.entangle;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.megasoft.config.Config;
 import com.megasoft.requests.GetRequest;
 import com.megasoft.requests.ImageRequest;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+/**
+ * Views a user's profile given his user Id and the tangle Id that redirected to the profile
+ * @author Almgohar
+ */
 public class ProfileActivity extends Activity {
-	private TextView edit;
+	
+	/**
+	 * The Button that redirects to the EditProfileActivity
+	 */
+	private Button edit;
+	
+	/**
+	 * The TextView that holds the user's name
+	 */
 	private TextView name;
-	private TextView descr;
+	
+	/**
+	 * The TextView that holds the user's description
+	 */
+	private TextView description;
+	
+	/**
+	 * The TextView that holds the user's credit/balance
+	 */
 	private TextView balance;
-	private TextView birthdate;
+	
+	/**
+	 * The TextView that holds the user's birth date
+	 */
+	private TextView birthDate;
+	
+	/**
+	 * The ImageView that indicates whether the user is verified
+	 */
 	private ImageView verifiedView;
+	
+	/**
+	 * The ImageView that holds the user's profile picture
+	 */
 	private ImageView profilePictureView;
-    LinearLayout layoutContainer;
-	SharedPreferences settings;
-	final Activity self = this;
-	int loggedInId;
+	
+	/**
+	 * The LinearLayout that holds the user's transactions
+	 */
+    private LinearLayout transactionsLayout;
+    
+    /**
+     * The preferences instance
+     */
+	private SharedPreferences settings;
+	
+	/**
+	 * The id of the logged in user
+	 */
+	private int loggedInId;
+	
+	/**
+	 * The tangle Id from which we were redirected
+	 */
+	private int tangleId;
+	
+	/**
+	 * The user Id whose profile we want to view
+	 */
+	private int userId;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
-			
-				try {
-
-					viewProfile();
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			
+		Intent intent = getIntent();
+		this.settings = getSharedPreferences(Config.SETTING, 0);
+		this.loggedInId = settings.getInt(Config.USER_ID, -1);
+		this.tangleId = intent.getIntExtra("tangle id", -1);
+		this.userId = intent.getIntExtra("user id", -1);		
+		
+		viewProfile();		
 	}
 
 	@Override
@@ -55,38 +103,43 @@ public class ProfileActivity extends Activity {
 		return true;
 	}
 	
-	public void viewProfile() throws JSONException {
-		edit = (TextView) findViewById(R.id.EditProfile);
+	/**
+	 * Initialize all views to link them to the XML views
+	 * calls the ViewInformation() method
+	 */
+	public void viewProfile() {
+		edit = (Button) findViewById(R.id.EditProfile);
 		name = (TextView) findViewById(R.id.nameView);
 		balance = (TextView) findViewById(R.id.balanceView);
-		descr = (TextView) findViewById(R.id.descriptionView);
-		birthdate = (TextView) findViewById(R.id.birthdateView);
+		description = (TextView) findViewById(R.id.descriptionView);
+		birthDate = (TextView) findViewById(R.id.birthDateView);
 		verifiedView = (ImageView) findViewById(R.id.verifiedView);
 		profilePictureView = (ImageView)findViewById(R.id.profileImage);
-		layoutContainer = (LinearLayout) this.findViewById(R.id.transactions_layout);
-		this.settings = getSharedPreferences(Config.SETTING, 0);
-		this.loggedInId = settings.getInt(Config.USER_ID, -1);
-		Intent intent = getIntent();
-		int tangleId = intent.getIntExtra("tangle id", -1);
-		int userId = intent.getIntExtra("user id", -1);
-		viewInformation(userId, tangleId);
+		transactionsLayout = (LinearLayout) this.findViewById(R.id.transactions_layout);
+		
+		viewInformation();
 	}
 	
-	public void viewInformation(final int userId, int tangleId) {
+	/**
+	 * Creates a JSon request asking for the required information
+	 * Retrieves the required information from the JSon response
+	 */
+	public void viewInformation() {
 		String link = "http://entangle2.apiary-mock.com/tangle/" 
 	+ tangleId + "/user/" + userId + "/profile";
 		GetRequest request = new GetRequest(link) {
 			protected void onPostExecute(String response) {
-				JSONObject jSon;
+				if (this.getStatusCode() == 200	) {
 				try {
+					JSONObject jSon;
 					jSon = new JSONObject(response);
 					JSONArray transactions = jSon.getJSONArray("transactions");
 					JSONObject information =  jSon.getJSONObject("information");
 					viewTransactions(transactions);
 					name.setText(information.getString("name"));
-					descr.setText("Description: " + information.getString("Description"));
+					description.setText("Description: " + information.getString("Description"));
 					balance.setText("Credit: " + information.getString("balance") + " points");
-					birthdate.setText("Birthdate: " + information.getString("birthdate"));
+					birthDate.setText("Birthdate: " + information.getString("birthdate"));
 					viewProfilePicture(information.getString("picture URL"));
 					boolean verified = information.getBoolean("verified");
 					if (verified) {
@@ -104,39 +157,54 @@ public class ProfileActivity extends Activity {
 					} catch (JSONException e) {
 						e.printStackTrace();
 						}
+				} else {
+					Toast toast = Toast.makeText(getApplicationContext(),"Some error happened.",Toast.LENGTH_SHORT);
+					toast.show();
+					}
 				}
 			};
 			request.execute();
-			}
+	}
 	
+	/**
+	 * Gets the user's transactions from a JSONArray and views them
+	 * @param JSONArray transactions
+	 */
 	public void viewTransactions(JSONArray transactions) {
-		TextView title = new TextView(self);
+		TextView title = new TextView(this);
 		title.setText("Transactions: ");
 		title.setTextSize(20);
-		layoutContainer.addView(title);
+		transactionsLayout.addView(title);
 		for(int i = 0; i < transactions.length(); i++) {
 			JSONObject object;
 			try {
 				object = transactions.getJSONObject(i);
-				TextView transaction = new TextView(self);
+				TextView transaction = new TextView(this);
 				String requester = object.getString("requesterName");
 				String request = object.getString("requestDescription");
 				String amount = object.getString("amount");
 				transaction.setText("Requester: " + requester 
 						+ '\n' + "Request: " + request
 						+ '\n' + "Amount: " + amount);
-				layoutContainer.addView(transaction);
+				transactionsLayout.addView(transaction);
 				} catch (JSONException e) {
 					e.printStackTrace();
 					}
 			}
 		}
 	
+	/**
+	 * Views the user's profile picture
+	 * @param String imageURL
+	 */
 	public void viewProfilePicture(String imageURL) {
             ImageRequest image = new ImageRequest(profilePictureView);
             image.execute(imageURL);
 	}
 	
+	/**
+	 * Redirects to the EditProfileActivity 
+	 */
 	public void goToEditProfile() {
 		Intent editProfile = new Intent(this, EditProfileActivity.class);
 		editProfile.putExtra("user id", loggedInId);
