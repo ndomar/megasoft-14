59e78ac28b0aa7e8b5790058a093c979bb7a239f
silvermist <sue.fairy@live.com>
2014-04-19 14:52:18 +0200

    Issue #128 - fix some backend bugs

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/RequestController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/RequestController.php
@@ -1,65 +1,75 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+namespace Megasoft\EntangleBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Megasoft\EntangleBundle\Entity\Session;
+use Megasoft\EntangleBundle\Entity\Request;
+use Megasoft\EntangleBundle\Entity\Tag;
 /**
  * Description of RequestController
  *
- * @author Asus
+ * @author Salma Khaled
  */
 class RequestController extends Controller{
-    public function createAction(Request $request , $tangleId)
+    public function createAction(\Symfony\Component\HttpFoundation\Request $request , $tangleId)
     {
         $doctrine = $this->getDoctrine();
         $json = $request->getContent();
+        $response = new JsonResponse();
         $json_array = json_decode($json,true);
         $sessionId = $request->headers->get('X-SESSION-ID');
         $sessionTable = $doctrine->getRepository('MegasoftEntangleBundle:Session');
-        $session = $sessionTable->findOneBy(array('id'=>$sessionId));
-        $userId = $session->getUserId();
+        $session = $sessionTable->findOneBy(array('sessionId'=>$sessionId));
+       // return new Response($sessionId);
+       /* if($session == null){
+        $response->setStatusCode(400);
+        return $response;
+        }*/
+       // $userId = $session->getUserId();
         $description = $json_array['description'];
         $tags = $json_array['tags'];
         $date = $json_array['date'];
         $deadLine = $json_array['deadLine'];
         $requestedPrice = $json_array['requestedPrice'];
         
-        $Request = new Request();
-        $Request->setDescription($description);
-        $Request->setStatus(1);
-        $Request->setTangleId($tangleId);
-        $Request->setDate($date);
-        $Request->setDeadLine($deadLine);
-        $Request->setUserId($userId);
-        $Request->setRequestedPrice($requestedPrice);
+        $newRequest = new Request();
+        $newRequest->setDescription($description);
+        $newRequest->setStatus(1);
+        $newRequest->setTangleId($tangleId);
+        $newRequest->setDate($date);
+        $newRequest->setDeadLine($deadLine);
+       // $newRequest->setUserId($userId);
+        $newRequest->setRequestedPrice($requestedPrice);
         
-        addTags($request , $tags);
+        $this->addTags($newRequest , $tags);
       
-        $doctrine->getManager()->persist($Request);
+        $doctrine->getManager()->persist($newRequest);
         $doctrine->getManager()->flush();
         
-        $response = new JsonResponse();
+        
         $response->setData(array('sessionId'=>$sessionId));
         $response->setStatusCode(201);
         return $response;
     }
     
     
-    public function addTags($Request , $tags){
-        $tagElements = explode("," , $tags);
-         $doctrine = $this->getDoctrine();
+    public function addTags($newRequest , $tags){
+       // $tagElements = explode("," , $tags);
+        $doctrine = $this->getDoctrine();
         $repo = $doctrine->getRepository('MegasoftEntangleBundle:Tag');
-        
-        for($i =0;i<$tagElements->length;$i++){
-        $tag = $repo->findOneBy(array('name'=>$tagElements[i]));
+        $arrlength = count($tags);
+        for($i =0;$i<$arrlength;$i++){
+        $tag = $repo->findOneBy(array('name'=>$tags[$i]));
         if($tag==null){
             $tag = new Tag();
-            $tag->setName($tagElements[$i]);
+            $tag->setName($tags[$i]);
         }
-            $Request->addTag($tag);
+            $newRequest->addTag($tag);
+            $doctrine->getManager()->persist($tag);
+            $doctrine->getManager()->flush();
             
         }
     }
