7724d4971e1ee45ab8e70ed521b1b9182d24ddc3
heba-aamer <heba93aamer@gmail.com>
2014-04-20 17:19:25 +0200

    Issue #164 - Adding the leaving request to the activity

--- Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -1,11 +1,14 @@
 package com.megasoft.entangle;
 
+import com.megasoft.requests.DeleteRequest;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
 
@@ -18,8 +21,9 @@ public class ProfileActivity extends Activity {
 	 * 
 	 * @author HebaAamer
 	 */
+	@SuppressWarnings("deprecation")
 	public void leaveTangle(View view) {
-
+		showDialog(0);
 	}
 
 	@Override
@@ -48,6 +52,33 @@ public class ProfileActivity extends Activity {
 	}
 
 	private void sendLeaveRequest() {
-
+		// to be changed to the tangleId
+		DeleteRequest leaveRequest = new DeleteRequest(
+				"http://entangle2.apiary-mock.com/tangle/"
+						+ getIntent().getIntExtra("tangleId", 0) + "user") {
+			public void onPostExecute(String response) {
+				if (response != null) {
+					if (getStatusCode() == 201) {
+						Toast.makeText(getBaseContext(),
+								"You left the tangle successfully",
+								Toast.LENGTH_LONG).show();
+						// redirect to the tangles stream Activity
+					} else if (getStatusCode() == 403) {
+						Toast.makeText(
+								getBaseContext(),
+								"Sorry, you are not allowed to leave the tangle",
+								Toast.LENGTH_LONG).show();
+					} else {
+						Toast.makeText(
+								getBaseContext(),
+								"Sorry, problem happened while leaving the tangle. Try again later",
+								Toast.LENGTH_LONG).show();
+					}
+				}
+			}
+		};
+		// to be changed to sessionId
+		leaveRequest.addHeader("X-SESSION-ID", "sessionId");
+		leaveRequest.execute();
 	}
 }
