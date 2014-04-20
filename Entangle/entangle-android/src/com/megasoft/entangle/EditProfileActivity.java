e398a4d7f18ab6eeecde1151102162a50a1c7f98
mennatarek <mennatallah.dawood@gmail.com>
2014-04-20 22:01:41 +0200

    Old commits lost while stash

--- Entangle/entangle-android/src/com/megasoft/entangle/EditProfileActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/EditProfileActivity.java
@@ -1,26 +1,24 @@
 package com.megasoft.entangle;
 
+import java.text.SimpleDateFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.megasoft.config.Config;
 import com.megasoft.entangle.R;
 import com.megasoft.requests.GetRequest;
 import com.megasoft.requests.PutRequest;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
-//import android.content.Intent;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-//import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class EditProfileActivity extends Activity {
 	SharedPreferences settings;
 	String sessionId;
-	// private CheckBox chk;
 	String emails;
 	String[] userEmails;
 	EditText currentDescription = (EditText) findViewById(R.id.CurrentDescription);
@@ -31,13 +29,15 @@ public class EditProfileActivity extends Activity {
 	EditText newPassword = (EditText) findViewById(R.id.AddNewPassword);
 	EditText confirmPassword = (EditText) findViewById(R.id.AddNewPasswordConfirmation);
 	EditText addedMail = (EditText) findViewById(R.id.AddedMail);
-
+	String oldBirthDate;
+	String[] splittedDate;
 	JSONObject whoamiResponse;
 
-	// Layout layout = (R.layout.activity_edit_profile);
+	@SuppressLint("SimpleDateFormat")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_profile);
 		this.settings = getSharedPreferences(Config.SETTING, 0);
 		this.sessionId = settings.getString(Config.SESSION_ID, "");
 		JSONObject whoamiRequest = new JSONObject();
@@ -53,59 +53,63 @@ public class EditProfileActivity extends Activity {
 			public void onPostExecute(String response) {
 				try {
 					whoamiResponse = new JSONObject(response);
-					// currentDescription.setText(whoamiResponse.getString("Description"));
-					// currentDOB.setText(whoamiResponse.getString("Date_of_birth"));
-					// emails = whoamiResponse.getString("Emails");
-					// userEmails = emails.split(",");
-
 				} catch (JSONException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				finish();
 			}
 		};
-		/*
-		 * for (int i = 0; i < userEmails.length; i++) { chk = new
-		 * CheckBox(this); chk.setText(userEmails[i]); addContentView(chk,
-		 * null);
-		 * 
-		 * }
-		 */
+
 		getRequest.addHeader(Config.API_SESSION_ID, sessionId);
 		getRequest.execute();
+		try {
+			SimpleDateFormat dateformatJava = new SimpleDateFormat("dd-MM-yyyy");
+			oldBirthDate = dateformatJava.format(whoamiResponse
+					.get("date_of_birth"));
+			splittedDate = oldBirthDate.split("-");
+			newday.setSelection(Integer.parseInt(splittedDate[0]) - 1);
+			newmonth.setSelection(Integer.parseInt(splittedDate[1]) - 1);
+			newyear.setSelection(Integer.parseInt(splittedDate[2]) - 1951);
+			currentDescription.setText(whoamiResponse.getString("Description"));
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
 		getIntent();
 		setContentView(R.layout.activity_edit_profile);
 	}
+
+	JSONObject putReJsonObject = new JSONObject();
 
 	public void saveAll(View view) {
 		PutRequest putRequest = new PutRequest(Config.API_BASE_URL + "/user/"
 				+ "edit");
 		putRequest.addHeader(Config.API_SESSION_ID, sessionId);
 		try {
-			whoamiResponse.put("Description", currentDescription.getText()
+			String date = newday.getSelectedItem().toString() + "-"
+					+ newmonth.getSelectedItem().toString() + "-"
+					+ newyear.getSelectedItem().toString();
+			putReJsonObject.put("newDescription", currentDescription.getText()
 					.toString());
-			whoamiResponse.put("day", newday.getSelectedItem().toString());
-			whoamiResponse.put("month", newmonth.getSelectedItem().toString());
-			whoamiResponse.put("year", newyear.getSelectedItem().toString());
-			whoamiResponse.put("current_password", currentPassword.getText()
+			putReJsonObject.put("new_date_of_birth", date);
+			putReJsonObject.put("current_password", currentPassword.getText()
 					.toString());
-			whoamiResponse
-					.put("new_password", newPassword.getText().toString());
-			whoamiResponse.put("confirm_password", confirmPassword.getText()
+			putReJsonObject.put("new_password", newPassword.getText()
 					.toString());
-			whoamiResponse.put("added_mail", addedMail.getText().toString());
+			putReJsonObject.put("confirm_password", confirmPassword.getText()
+					.toString());
+			putReJsonObject.put("added_mail", addedMail.getText().toString());
 
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		putRequest.setBody(whoamiResponse);
+		putRequest.addHeader(Config.API_SESSION_ID, sessionId);
+		putRequest.setBody(putReJsonObject);
 		putRequest.execute();
-		/*
-		 * Intent viewEditedProfile = new Intent(this, MyProfile.class);
-		 * startActivity(viewEditedProfile);
-		 */
+
+		Intent viewEditedProfile = new Intent(this, MyProfile.class);
+		startActivity(viewEditedProfile);
 
 	}
 
--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
@@ -8,36 +8,47 @@
 
 namespace Megasoft\EntangleBundle\Controller;
 
-use Megasoft\EntangleBundle\Entity\User;
 use Megasoft\EntangleBundle\Entity\UserEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class User extends Controller {
+class UserController extends Controller {
 
     public function edit(Request $r) {
         $requestContent = $r->getContent();
         $jsonArray = json_decode($requestContent, true);
-        Megasoft\EntangleBundle\Entity\User:: $user = $jsonArray['user'];
-        $newDescription = $jsonArray['newDescription'];
+        $sessionId = $jsonArray['session_id'];
+        $sesionRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:Session');
+        $currentSession = $sesionRepo->findOneBy(array('sessionId' => $sessionId));
+        $user = $currentSession->getUser();
+        $newDescription = $jsonArray['description'];
         if (strcmp($user->getUserBio(), $newDescription) != 0) {
             $user->setUserBio($newDescription);
         }
-        $newPassword = $jsonArray['newPassword'];
-        $confirmNewPassword = $jsonArray['confirmNewPassword'];
-        $givenCurrentPassword = $jsonArray['givenCurrentPassword'];
+        $newPassword = $jsonArray['new_password'];
+        $confirmNewPassword = $jsonArray['confirm_password'];
+        $givenCurrentPassword = $jsonArray['current_password'];
         $currentPassword = $user->getPassword();
         if ($givenCurrentPassword == $currentPassword && $newPassword == $confirmNewPassword) {
-            $user->setPassword($newPassword);
+            $user->setPassword(md5($newPassword));
         }
         $newDateOfBirth = $jsonArray['newDateOfBirth'];
-        if (strcmp($newDateOfBirth, $user->getBirthDate()) != 0) {
+        $dateGiven = date($user->getBirthDate());
+        $dateString = $dateGiven->format("m-d-Y");
+        if (strcmp($newDateOfBirth, $dateString) != 0) {
             $user->setBirthDate($newDateOfBirth);
         }
-        $newMail = new UserEmail();
-        $newMail->setEmail($jsonArray['newMail']);
-        $newMail->setUser($user);
-        $user->addEmail($newMail);
+        $doctrineManger = $this->getDoctrine()->getManager();
+
+        if (filter_var($jsonArray['added_mail'], FILTER_VALIDATE_EMAIL)) {
+            $newMail = new UserEmail();
+            $newMail->setEmail($jsonArray['added_mail']);
+            $newMail->setUser($user);
+            $user->addEmail($newMail);
+            $doctrineManger->persist($newMail);
+        }
+        $doctrineManger->persist($user);
+        $doctrineManger->flush();
     }
 
     public function deleteSecondaryEmail(Request $r) {
@@ -46,6 +57,9 @@ class User extends Controller {
         $user = $jsonArray['user'];
         $deletedMail = $jsonArray['deletedMail'];
         $user->removeEmail($deletedMail);
+        $doctrineManger = $this->getDoctrine()->getManager();
+        $doctrineManger->persist($user);
+        $doctrineManger->flush();
     }
 
 }
