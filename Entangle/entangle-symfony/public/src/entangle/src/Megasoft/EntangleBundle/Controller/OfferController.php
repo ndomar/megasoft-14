2723d59e1b3bed76d20a6f15c09bf1edf0c5e2a4
sak93 <sarakhodeir@gmail.com>
2014-04-21 01:56:42 +0300

    Issue #159 - add verification function

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
@@ -24,11 +24,45 @@ class OfferController extends Controller {
         $json = $request->getContent();
         $json_array = json_decode($json, true);
         $offerId = $json_array['offerId'];
-        //$this->verify($offerId);
+        $this->verify($offerId);
         $response = new Response();
         $response->setStatusCode(200);
         return $response;
     }
 
-    
+    public function verify($offerId) {
+        $doctrine = $this->getDoctrine();
+        $offerRepo = $doctrine->getRepository('MegasoftEntangleBundle:Offer');
+        $offer = $offerRepo->findOneBy(array('id' => $offerId));
+        if ($offer->getStatus() === 0) {
+            $requestId = $offer->getRequestId();
+            $requestRepo = $doctrine->getRepository('MegasoftEntangleBundle:Request');
+            $request = $requestRepo->findOneBy(array('id' => $requestId));
+            $requesterId = $request->getUserId();
+            if ($request->getStatus() === 0) {
+                $tangleId = $request->getTangleId();
+                $price = $offer->getRequestedPrice();
+                $userTangle = $doctrine->getRepository('MegasoftEntangleBundle:UserTangle');
+                $requester = $userTangle->findOneBy(array('tangleId' => $tangleId, 'userId' => $requesterId));
+                $requesterBalance = $requester->getCredit();
+                if ($requesterBalance >=$price) {
+                    printf("verified");
+                    $request->setStatus(1); //check this;
+                    $this->getDoctrine()->getManager()->persist($request);
+                    $requester->setCredit($requesterBalance - $price);
+                    $this->getDoctrine()->getManager()->persist($requester);
+                    $offer->setStatus(1);
+                    $this->getDoctrine()->getManager()->persist($offer);
+                    $this->getDoctrine()->getManager()->flush();
+                } else {
+                    printf("requester doesn't have enough balance");
+                }
+            } else {
+                printf("request closed");
+            }
+        } else {
+            printf("offer closed");
+        }
+    }
+
 }
