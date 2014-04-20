2bc7e4f8b7478c5031628ccc7113b2c18be48dbf
maisara <maisara@maisara-PC.(none)>
2014-04-20 21:35:42 +0200

    Issue #168 - fix conflicts

--- Entangle/entangle-android/src/com/megasoft/config/Config.java
+++ Entangle/entangle-android/src/com/megasoft/config/Config.java
@@ -1,11 +1,10 @@
 package com.megasoft.config;
 
 public class Config {
-	
+
 	public static final String API_BASE_URL = "http://entangle2.apiary-mock.com";
 	public static final String API_SESSION_ID = "X-SESSION-ID";
-	
-	
+
 	public static final String SETTING = "SETTINGS";
 	public static final String SESSION_ID = "sessionId";
 }
