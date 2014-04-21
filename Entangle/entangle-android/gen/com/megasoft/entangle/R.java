87cfbf4aa7c81222cb450efb814e4ad6c1408039
Salma Amr <salmaumr@gmail.com>
2014-04-21 22:39:41 +0200

    Issue #174 - Add R.java File

--- Entangle/entangle-android/gen/com/megasoft/entangle/R.java
+++ Entangle/entangle-android/gen/com/megasoft/entangle/R.java
@@ -24,43 +24,68 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int claimbutton=0x7f08000e;
-        public static final int fromLabel=0x7f080002;
-        public static final int fromText=0x7f080003;
-        public static final int linearLayout=0x7f080000;
-        public static final int mssgLabel=0x7f080006;
-        public static final int mssgText=0x7f080008;
-        public static final int pageNameLabel=0x7f080009;
-        public static final int send=0x7f08000a;
-        public static final int subjectLabel=0x7f080005;
-        public static final int subjectText=0x7f080007;
-        public static final int textView1=0x7f08000c;
-        public static final int textView2=0x7f08000d;
-        public static final int textView3=0x7f08000b;
-        public static final int toLabel=0x7f080001;
-        public static final int toText=0x7f080004;
+        public static final int action_settings=0x7f080016;
+        public static final int claimbutton=0x7f080015;
+        public static final int fromLabel=0x7f08000c;
+        public static final int fromText=0x7f08000d;
+        public static final int invite_already_in_tangle=0x7f080006;
+        public static final int invite_already_in_tangle_text=0x7f080005;
+        public static final int invite_emails=0x7f080000;
+        public static final int invite_entangle_members=0x7f080004;
+        public static final int invite_entangle_members_text=0x7f080003;
+        public static final int invite_invalid_emails=0x7f080008;
+        public static final int invite_invalid_emails_text=0x7f080007;
+        public static final int invite_invitation_message=0x7f080009;
+        public static final int invite_not_members=0x7f080002;
+        public static final int invite_not_members_text=0x7f080001;
+        public static final int linearLayout=0x7f08000a;
+        public static final int mssgLabel=0x7f080010;
+        public static final int mssgText=0x7f080012;
+        public static final int pageNameLabel=0x7f080013;
+        public static final int send=0x7f080014;
+        public static final int subjectLabel=0x7f08000f;
+        public static final int subjectText=0x7f080011;
+        public static final int toLabel=0x7f08000b;
+        public static final int toText=0x7f08000e;
     }
     public static final class layout {
-        public static final int claimform=0x7f030000;
-        public static final int request=0x7f030001;
+        public static final int activity_invite_users=0x7f030000;
+        public static final int activity_invite_users_confirmations=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int claimform=0x7f030003;
+        public static final int request=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int invite_users=0x7f070000;
+        public static final int invite_users_confirmations=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_email=0x7f05000e;
+        public static final int already_in_the_tangle=0x7f050015;
         public static final int app_name=0x7f050000;
         public static final int claim_button=0x7f050005;
+        public static final int entangle_members=0x7f050014;
         public static final int fromMail=0x7f050007;
+        public static final int hello_world=0x7f05000c;
+        public static final int invite=0x7f050018;
+        public static final int invite_continue=0x7f05000f;
+        public static final int invite_write_the_invitation_message=0x7f050017;
         public static final int mssg=0x7f050009;
         public static final int name=0x7f05000a;
+        public static final int not_members=0x7f050013;
         public static final int offerer_is_here=0x7f050003;
         public static final int request_is_here=0x7f050002;
         public static final int requester_is_here=0x7f050004;
         public static final int send=0x7f05000b;
         public static final int subject=0x7f050008;
+        public static final int title_activity_invite_users=0x7f05000d;
+        public static final int title_activity_invite_users_confirmation=0x7f050011;
+        public static final int title_activity_invite_users_confirmations=0x7f050012;
         public static final int toMail=0x7f050006;
+        public static final int user_email=0x7f050010;
+        public static final int wrong_emails=0x7f050016;
     }
     public static final class style {
         /** 
