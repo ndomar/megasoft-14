743e401fbac67be6dcdfb183dc8141b5e23534ea
sak93 <sarakhodeir@gmail.com>
2014-04-21 13:37:16 +0300

    Issue #159 - bug fix and add documentation

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
@@ -20,48 +20,63 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class OfferController extends Controller {
 
+    /**
+     * this recieves a request and calls verify to check if it can accept the offer  
+     * @param  Request $request
+     * @return Response $response returns 201 or 409 status code and message depending on verification
+     * @author sak9
+     */
     public function acceptOfferAction(Request $request) {
         $json = $request->getContent();
         $json_array = json_decode($json, true);
         $offerId = $json_array['offerId'];
-        $this->verify($offerId);
-        $response = new Response();
-        $response->setStatusCode(200);
+        $verificationMessage = $this->verify($offerId);
+        if ($verificationMessage == "Offer Accepted.") {
+            $response = new Response($verificationMessage, 201);
+        } else {
+            $response = new Response($verificationMessage, 409);
+        }
         return $response;
     }
 
+    /**
+     * this recieves an offerId and checks if it can be accepted, if it can it accepts it and updates all fields in tables
+     * @param  Int $offerId 
+     * @return String either a success or error message
+     * @author sak9
+     */
     public function verify($offerId) {
         $doctrine = $this->getDoctrine();
         $offerRepo = $doctrine->getRepository('MegasoftEntangleBundle:Offer');
         $offer = $offerRepo->findOneBy(array('id' => $offerId));
-        if ($offer->getStatus() === 0) {
-            $requestId = $offer->getRequestId();
-            $requestRepo = $doctrine->getRepository('MegasoftEntangleBundle:Request');
-            $request = $requestRepo->findOneBy(array('id' => $requestId));
-            $requesterId = $request->getUserId();
-            if ($request->getStatus() === 0) {
+        $requestId = $offer->getRequestId();
+        $requestRepo = $doctrine->getRepository('MegasoftEntangleBundle:Request');
+        $request = $requestRepo->findOneBy(array('id' => $requestId));
+        $requesterId = $request->getUserId();
+        if ($request->getStatus() === 0) {
+            if ($offer->getStatus() === 0) {
                 $tangleId = $request->getTangleId();
                 $price = $offer->getRequestedPrice();
                 $userTangle = $doctrine->getRepository('MegasoftEntangleBundle:UserTangle');
                 $requester = $userTangle->findOneBy(array('tangleId' => $tangleId, 'userId' => $requesterId));
                 $requesterBalance = $requester->getCredit();
-                if ($requesterBalance >=$price) {
-                    printf("verified");
-                    $request->setStatus(1); //check this;
-                    $this->getDoctrine()->getManager()->persist($request);
+                if ($requesterBalance >= $price) {
+                    $request->setStatus(1);
                     $requester->setCredit($requesterBalance - $price);
-                    $this->getDoctrine()->getManager()->persist($requester);
                     $offer->setStatus(1);
-                    $this->getDoctrine()->getManager()->persist($offer);
-                    $this->getDoctrine()->getManager()->flush();
+                    $doctrine->getManager()->persist($request);
+                    $doctrine->getManager()->persist($requester);
+                    $doctrine->getManager()->persist($offer);
+                    $doctrine->getManager()->flush();
+                    return "Offer Accepted.";
                 } else {
-                    printf("requester doesn't have enough balance");
+                    return "Error: Not enough balance.";
                 }
             } else {
-                printf("request closed");
+                return "Error: Offer Closed.";
             }
         } else {
-            printf("offer closed");
+            return "Error: Request Closed.";
         }
     }
 
