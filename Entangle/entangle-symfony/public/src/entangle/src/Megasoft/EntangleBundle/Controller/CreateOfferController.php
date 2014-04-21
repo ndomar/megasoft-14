a7f5e97394573b659d0c0d125f6972a42ac316c8
silvermist <sue.fairy@live.com>
2014-04-21 12:20:07 +0200

    Issue #166 - add initial controller code

---
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/CreateOfferController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Megasoft\EntangleBundle\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Megasoft\EntangleBundle\Entity\Offer;
+/**
+ * Description of CreateOfferController
+ *
+ * @author Salma Khaled
+ */
+class CreateOfferController extends Controller {
+    public function createOfferAction(Request $request , $tangleId , $requestId) {
+        $doctrine = $this->getDoctrine();
+        $json = $request->getContent();
+        $response = new JsonResponse();
+        $json_array = json_decode($json, true);
+        $sessionId = $request->headers->get('X-SESSION-ID');
+        if ($sessionId == null) {
+            $response->setStatusCode(400);
+            return $response;
+        }
+        $sessionTable = $doctrine->getRepository('MegasoftEntangleBundle:Session');
+        $session = $sessionTable->findOneBy(array('sessionId' => $sessionId));
+        if ($session == null) {
+            $response->setStatusCode(401);
+            return $response;
+        }
+        $userTable = $doctrine->getRepository('MegasoftEntangleBundle:User');
+        $userId = $session->getUserId();
+        $user = $userTable->findOneBy(array('id' => $userId));
+        $requestTable = $doctrine->getRepository('MegasoftEntangleBundle:Request');
+        $theRequestId = (int) $requestId;
+        $tangleRequest = $requestTable->findOneBy(array('id' => $theRequestId));
+        if ($tangleRequest == null ) {
+            $response->setStatusCode(401);
+            return $response;
+        }
+        if ($tangleRequest->getDeleted()){
+            $response->setStatusCode(400);
+            return $response;
+        }
+        $description = $json_array['description'];
+        $date = $json_array['date'];
+        $dateFormated = new \DateTime($date);
+        $deadLine = $json_array['deadLine'];
+        $deadLineFormated = new \DateTime($deadLine);
+        $requestedPrice = $json_array['requestedPrice'];
+        
+        $newOffer = new Offer();
+        $newOffer->setRequestedPrice($requestedPrice);
+        $newOffer->setDate($dateFormated);
+        $newOffer->setDescription($description);
+        $newOffer->setExpectedDeadline($deadLineFormated);
+        $newOffer->setUser($user);
+        $newOffer->setRequest($tangleRequest);
+        $newOffer->setStatus(1);
+        
+        $doctrine->getManager()->persist($newOffer);
+        $doctrine->getManager()->flush();
+
+        $response->setData(array('sessionId' => $sessionId));
+        $response->setStatusCode(201);
+        return $response;
+        
+    } 
+}
