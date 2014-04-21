7bdd25d12c33dedcbcc7d8e90f5e74ca005c5285
silvermist <sue.fairy@live.com>
2014-04-21 10:03:53 +0200

    Issue #166 - fix code format

--- Entangle/entangle-android/src/com/megasoft/entangle/CreateOfferActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/CreateOfferActivity.java
@@ -20,35 +20,34 @@ import android.widget.TextView;
 
 public class CreateOfferActivity extends Activity {
 
-	
 	SharedPreferences settings;
 	String sessionId;
 	EditText description;
 	EditText requestedPrice;
 	Button Post;
 	JSONObject json = new JSONObject();
-	
+
 	int deadLineYear;
-	
+
 	int deadLineMonth;
-	
+
 	int deadLineDay;
-	
+
 	TextView dateDisplay;
-	
+
 	Button pickDate;
 	CheckBox checkBox;
-	
+
 	static final int DATE_DIALOG_ID = 0;
-	
+
 	final Calendar calendar = Calendar.getInstance();
-	
+
 	final int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-	
+
 	final int currentMonth = calendar.get(Calendar.MONTH);
-	
+
 	final int currentYear = calendar.get(Calendar.YEAR);
-	
+
 	final String date = currentDay + "/" + (currentMonth + 1) + "/"
 			+ currentYear;
 
@@ -74,9 +73,9 @@ public class CreateOfferActivity extends Activity {
 		final String currentDateTime = date + " " + calendar.get(Calendar.HOUR)
 				+ ":" + calendar.get(Calendar.MINUTE) + ":"
 				+ calendar.get(Calendar.SECOND);
-		
+
 	}
-	
+
 	OnFocusChangeListener focusListener = new OnFocusChangeListener() {
 		public void onFocusChange(View view, boolean hasFocus) {
 			EditText editText = (EditText) view;
@@ -85,24 +84,22 @@ public class CreateOfferActivity extends Activity {
 					Post.setEnabled(false);
 				}
 			} else {
-					checkBox.setChecked(false);
-				}
-			}
-		};
-	
-	
-	       public void itemClicked(View view)  {
-	    	   View focusedView = getCurrentFocus();
-	   		focusedView.clearFocus();
-	    	   CheckBox checkBox = (CheckBox) view;
-			if(isEmpty(description) | isEmpty(requestedPrice)){
 				checkBox.setChecked(false);
-			} else {
-				checkBox.setChecked(true);
-				Post.setEnabled(true);
 			}
 		}
-	
+	};
+
+	public void itemClicked(View view) {
+		View focusedView = getCurrentFocus();
+		focusedView.clearFocus();
+		CheckBox checkBox = (CheckBox) view;
+		if (isEmpty(description) | isEmpty(requestedPrice)) {
+			checkBox.setChecked(false);
+		} else {
+			checkBox.setChecked(true);
+			Post.setEnabled(true);
+		}
+	}
 
 	private boolean isEmpty(EditText editText) {
 		if (editText.getText().toString().length() == 0) {
