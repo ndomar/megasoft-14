966769e0e704f2a488a8575a131dd34d95fa41a4
sak93 <sarakhodeir@gmail.com>
2014-04-22 08:53:09 +0300

    Issue #159 - remove autogenerated comment

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
@@ -7,12 +7,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 class OfferController extends Controller {
 
     /**
