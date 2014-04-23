f2624658f3b2ed0dd9413bfb70cbfaf702be979a
maisara <maisara@maisara-PC.(none)>
2014-04-23 20:04:33 +0200

    Issue #168 - finalizing the login backend

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
@@ -2,9 +2,10 @@
 
 namespace Megasoft\EntangleBundle\Controller;
 
+use Megasoft\EntangleBundle\Entity\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class UserController extends Controller {
 
@@ -26,12 +27,12 @@ class UserController extends Controller {
 
     /**
      * Validates the username and password from request and returns sessionID
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response $response
      * 
      * @author maisaraFarahat
      */
-    public function loginAction(Symfony\Component\HttpFoundation\Request $request) {
+    public function loginAction(\Symfony\Component\HttpFoundation\Request $request) {
         $response = new JsonResponse();
         $badReq = "bad request";
         if (!$request) {
@@ -44,33 +45,41 @@ class UserController extends Controller {
             return $response;
         }
         $json_array = json_decode($json, true);
-        $username = $json_array['username'];
+        $name = $json_array['name'];
         $password = md5($json_array['password']);
 
-        if (!$username) {
-            $response->setStatusCode(400, "missing username");
+        if (!$name) {
+            $response->setStatusCode(400, "missing name");
             return $response;
         }
         if (!$password) {
             $response->setStatusCode(400, "missing password");
             return $response;
         }
-        if (strstr("\"", $username) || strstr("'", $username)) {
-            $response->setStatusCode(400, "username has special characters");
+        if (strstr("\"", $name) || strstr("'", $name)) {
+            $response->setStatusCode(400, "name has special characters");
             return $response;
         }
         $sessionId = $this->generateSessionId(30);
 
         $repo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:User');
-        $user = $repo->findOneBy(array('username' => $username, 'password' => $password));
+        $user = $repo->findOneBy(array('name' => $name, 'password' => $password));
 
-        $user->setSessionId($sessionId);
+        $session = new Session();
+        $session->setSessionId($sessionId);
+        $session->setUser($user);
+        $session->setUserId($user->getId());
+        $session->setCreated(new \DateTime('tomorrow'));
+        $session->setExpired(0);
+        $session->setRegId("ToAvoidNull");
+        $session->setDeviceType("Galaxy S3");
 
-
+        $user->addSession($session);
 
         $this->getDoctrine()->getManager()->persist($user);
-        $this->getDoctrine()->getManager()->flush();
+        $this->getDoctrine()->getManager()->persist($session);
 
+        $this->getDoctrine()->getManager()->flush();
         $response->setData(array('sessionId' => $sessionId));
         $response->setStatusCode(201);
         return $response;
