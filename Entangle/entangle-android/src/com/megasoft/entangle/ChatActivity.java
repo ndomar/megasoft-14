10b336a0610b25d1e9cc3716edc6f17071fc26c6
naderdavid <emailandmail@yahoo.com>
2014-04-23 22:51:49 +0200

    Issue #167 - adding send a message request

--- Entangle/entangle-android/src/com/megasoft/entangle/ChatActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ChatActivity.java
@@ -1,6 +1,13 @@
 package com.megasoft.entangle;
 
+import java.util.Calendar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.megasoft.config.Config;
+import com.megasoft.requests.GetRequest;
+import com.megasoft.requests.PostRequest;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -11,6 +18,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ChatActivity extends Activity {
 	public Button send;
@@ -31,10 +39,13 @@ public class ChatActivity extends Activity {
 		messageBox = (EditText) this.findViewById(R.id.message);
 		this.setSendButton(findViewById(android.R.id.content));
 		chatLayout = (LinearLayout) this.findViewById(R.id.chat);
+		this.getChat();
 	}
+	
 	/**
 	 * this method defines what happens when send button is clicked
 	 * @param view  the view of this activity
+	 * @return none
 	 * @author Nader Nessem
 	 */
 	private void setSendButton(View view) {
@@ -42,15 +53,60 @@ public class ChatActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-			String sentMessage = messageBox.getText().toString();
-			TextView message= new TextView(current);
-			message.setText(sentMessage);
-			chatLayout.addView(message);
+				String sentMessage = messageBox.getText().toString();
+				TextView message= new TextView(current);
+				message.setText(sentMessage);
+				chatLayout.addView(message);
+				JSONObject json = new JSONObject();
+				try {
+					//how to know the sender id
+					json.put("senderId", 123);
+					json.put("offerId",offerId);
+					json.put("body", sentMessage);
+					json.put("date", Calendar.getInstance().getTime());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				PostRequest sendMessageRequest = new PostRequest(Config.API_BASE_URL + "/offer/" + 
+				offerId + "/message"){
+		            protected void onPostExecute(String response) { 
+		                 if( this.getStatusCode() == 201 ){
+		                    Toast sucessToast =Toast.makeText(current,"Message sent Sucessfully"
+		                    		, Toast.LENGTH_SHORT);
+		                    sucessToast.show();
+		                  }else if( this.getStatusCode() == 400 ) {
+		                	  Toast failToast =Toast.makeText(current,"Unable to send the message"
+		                			  , Toast.LENGTH_SHORT);
+		                	  failToast.show();
+		                  }
+		             }
+		        };
+		        sendMessageRequest.setBody(json); 
+		        sendMessageRequest.addHeader("X-SESSION-ID", sessionId); 
+		        sendMessageRequest.execute();
 			}
 		});
 	}
+	/**
+	 * gets the previous chat between the requester and the offerer
+	 * @param none
+	 * @return none
+	 * @author Nader Nessem
+	 */
+	private void getChat(){
+		GetRequest getChatRequest = new GetRequest(Config.API_BASE_URL + "/offer/" + 
+				offerId + "/message"){
+			 protected void onPostExecute(String response){
+				 if( this.getStatusCode() == 200){
+					 
+				 }
+	                   
+				 
+			 }
+			
+		};
+	}
 	
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
