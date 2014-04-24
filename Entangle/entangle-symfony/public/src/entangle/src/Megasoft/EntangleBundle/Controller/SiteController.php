17b562e942248a08ffa3a3a81ad6c400618ee025
MohamedBassem <medoox240@gmail.com>
2014-04-25 00:22:05 +0200

    Issue #230 - Adding documentation

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/SiteController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/SiteController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SiteController extends Controller
 {
+    /**
+     * The action that renders the homepage
+     * @return response
+     * @author MohamedBassem
+     */
     public function indexAction()
     {
         return $this->render('MegasoftEntangleBundle:Site:index.html.twig');
