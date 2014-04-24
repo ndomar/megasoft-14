a17851ba12021522f05e1dfb1b48ba6d893f841a
Mohamed Shaban <mohamed19936@gmail.com>
2014-04-24 05:44:03 +0200

    #178 delete wrong commited files

--- Entangle/entangle-android/src/com/megasoft/entangle/GcmIntentService.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/GcmIntentService.java
@@ -36,7 +36,7 @@ public class GcmIntentService extends IntentService {
 		String body = intent.getExtras().getString("body");
 
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-				new Intent(this, CreateNotificationActivity.class), 0);
+				new Intent(this, MainActivity.class), 0);
 
 		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
 				this).setSmallIcon(R.drawable.common_signin_btn_icon_dark)
