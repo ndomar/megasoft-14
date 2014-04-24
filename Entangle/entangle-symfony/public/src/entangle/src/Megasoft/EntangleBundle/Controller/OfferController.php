6df561911095faaa1d74499b0e88fd1bedda76a4
Mansour <mansoursaid001@hotmail.com>
2014-04-24 17:34:08 +0200

    Issue #188 - Working story without documentation

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class OfferController extends Controller {
 
-    public function changeOfferPriceAction(Request $request, $requestid, $offerid) {
+    public function changeOfferPriceAction(Request $request, $offerid) {
         $sessionId = $request->headers->get('X-SESSION-ID');
         $sesionRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:Session');
         $session = $sesionRepo->findOneBy(array('sessionId' => $sessionId));
@@ -22,29 +22,36 @@ class OfferController extends Controller {
         if ($sessionExpired) {
             return new Response("Session expired", 440);
         }
-        $requestRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:Request');
-        $tangleRequest = $requestRepo->findOneBy(array('id' => $requestid));
-        if ($tangleRequest == null) {
-            return new Response("Not found", 404);
-        }
         $offerRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:Offer');
         $requestOffer = $offerRepo->findOneBy(array('id' => $offerid));
         if ($requestOffer == null) {
             return new Response("Not found", 404);
         }
-        if (($requestOffer->getRequestId()) != $requestid) {
-            return new Response("Unauthorized", 401);
-        }
         if (($session->getUserId()) != ($requestOffer->getUserId())) {
             return new Response("Unauthorized", 401);
         }
-        if(($requestOffer->getStatus) == (\Megasoft\EntangleBundle\Entity\Offer::ACCEPTED)){
+        if(($requestOffer->getStatus()) == (\Megasoft\EntangleBundle\Entity\Offer::ACCEPTED)){
             return new Response("Offer is already accepted", 401);
+        }
+        $tangleRequest = $requestOffer->getRequest();
+        if ($tangleRequest == null) {
+            return new Response("Bad Request", 400);
+        }
+        if(($tangleRequest->getId()) != ($requestOffer->getRequestId())){
+            return new Response("Bad Request", 400);
         }
         $json = $request->getContent();
         $json_array = json_decode($json, true);
         $newOfferPrice = $json_array['newPrice'];
+        if($newOfferPrice == null){
+            return new Response("Bad Request", 400);
+        }
+        if(($requestOffer->getRequestedPrice()) == $newOfferPrice){
+            return new Response("Same price, enter a new one", 400);
+        }
         $requestOffer->setRequestedPrice($newOfferPrice);
+        $this->getDoctrine()->getManager()->persist($requestOffer);
+        $this->getDoctrine()->getManager()->flush();
         return new Response('Price changed', 200);
     }
 
