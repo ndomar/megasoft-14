bbd15326e249ba4069a367471b8add69742d0337
Alaa Maher <almgohar@gmail.com>
2014-04-25 20:07:23 +0200

    Fix Conflict - #330

---
+++ Entangle/entangle-android/src/com/megasoft/entangle/ProfileActivity.java
@@ -0,0 +1,32 @@
+package com.megasoft.entangle;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+
+public class ProfileActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+		ProfileFragment profile = new ProfileFragment();
+		Bundle bundle = new Bundle();
+		bundle.putInt("tangleId", getIntent().getIntExtra("tangleId", 0));
+		bundle.putInt("userId", getIntent().getIntExtra("userId", 0));
+		profile.setArguments(bundle);
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction.add(R.id.profile_layout, profile);
+		transaction.commit();	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+
+}
