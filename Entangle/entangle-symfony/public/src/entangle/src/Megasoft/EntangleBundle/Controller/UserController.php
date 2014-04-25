0479753c0126c104e4fda544f8ab0cd4a6121e9f
Alaa Maher <almgohar@gmail.com>
2014-04-25 23:50:08 +0200

    fix error - #330

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
@@ -127,10 +127,12 @@ class UserController extends Controller {
                 $requesterName =$offer->getRequest()->getUser()->getName();
                 $requestDescription = $offer->getRequest()->getDescription();
                 $amount = $offer->getTransaction()->getFinalPrice();
+                $requestId = $offer->getRequest().getId();
+                $requesterId = $offer->getRequest().getUserId();
                 $transactions[] = array('offerId'=>$offer->getId(),
                     'requesterName'=> $requesterName,
                     'requestDescription'=>$requestDescription,
-                    'amount'=>$amount);
+                    'amount'=>$amount, 'requestId'=>$requestId, 'requesterId'=>$requesterId);
             }           
         }
         return $transactions;    
