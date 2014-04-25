36c81ecf988e16d16a10d7315fdbf462404f0404
Omar ElAzazy <oa12gb@gmail.com>
2014-04-25 21:37:49 +0200

    Issue #331 - Add constants in config file

--- Entangle/entangle-android/src/com/megasoft/config/Config.java
+++ Entangle/entangle-android/src/com/megasoft/config/Config.java
@@ -9,4 +9,8 @@ public class Config {
 	public static final String SETTING = "SETTINGS";
 	public static final String SESSION_ID = "sessionId";
 	public static final String USER_ID = "0";
+	public static final String TANGLE_ID = "tangleId";
+	public static final String MEMBER_NAME = "memberName";
+	public static final String MEMBER_BALANCE = "memberBalance";
+	public static final String MEMBER_AVATAR_URL = "memberAvatarURL";
 }
