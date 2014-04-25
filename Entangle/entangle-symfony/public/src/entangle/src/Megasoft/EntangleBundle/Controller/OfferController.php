bfb70fb6225529c94c35f923cf8c2710f86a3851
mohamedzayan19 <mohamed.zayan19@gmail.com>
2014-04-25 22:41:14 +0200

    Issue #202 -Add Minor fixes

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/OfferController.php
@@ -3,7 +3,6 @@
 namespace Megasoft\EntangleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Megasoft\EntangleBundle\Entity\Request;
