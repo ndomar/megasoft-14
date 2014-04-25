7e3fff1535c12930a6da35e35136aee1a2e1e62d
heba-aamer <heba93aamer@gmail.com>
2014-04-25 18:10:02 +0200

    Modify launching in Android Manifest

--- Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
@@ -15,12 +15,8 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Intent intent = new Intent(this, RequestActivity.class);
-		startActivity(intent);
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_main);  
-//		startActivity((new Intent(this,TangleStreamActivity.class)));
 	}
 
 	@Override
