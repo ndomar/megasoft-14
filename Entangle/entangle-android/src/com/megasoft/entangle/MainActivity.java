108eb3fb479761d3318e79edcefa68d69f2b0551
Omar ElAzazy <oa12gb@gmail.com>
2014-04-25 21:37:12 +0200

    Issue #331 - Make main activity showing the member list

--- Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
@@ -1,25 +1,28 @@
 package com.megasoft.entangle;
 
-import org.apache.http.client.methods.HttpPost;
-
-import com.megasoft.entangle.acceptPendingInvitation.ManagePendingInvitationActivity;
-import com.megasoft.entangle.viewtanglelsit.TangleStreamActivity;
-import com.megasoft.requests.PostRequest;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.megasoft.config.Config;
+
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);  
-		startActivity((new Intent(this,TangleStreamActivity.class)));
+		
+		MemberListFragment memberListFragment = new MemberListFragment();
+    	
+    	Bundle bundle = new Bundle();
+    	bundle.putInt(Config.TANGLE_ID, 1);
+    	memberListFragment.setArguments(bundle);
+    	
+    	getSupportFragmentManager().beginTransaction().add(R.id.member_list_placeholder, memberListFragment).commit();
+  
 	}
-
+ 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
