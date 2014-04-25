98770b61e67996a91f32f76a808118c625e7107d
amrelzanaty <amrelzanaty@zanatypcVPCSB36FA.(none)>
2014-04-26 01:12:07 +0200

    issue #181 fixing errors

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/MailController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/MailController.php
@@ -9,6 +9,7 @@ class MailController extends Controller {
 
     /**
      * @return Response
+     * @author amrelzanaty
      */
     public function SendEmailAction() {
         $this->sendMail(1, "Hello world ", "Welcome to Entangle");
@@ -17,23 +18,31 @@ class MailController extends Controller {
 
     /**
      * this function sends an email notification to user with userID
-     * @param  Int      $userid    user ID
-     * @param  String   $subject   The subject of the email notification
-     * @return string   $body      The body of the email notification
+     * @param  Int       $userid    user ID
+     * @param  String    $subject   The subject of the email notification
+     * @param  boolean   $maildeleted decides if the mail is deleted or not
+     * @param  boolean   $accepmail decides if the the user accepts mail or not
+     * @return string    $body  The body of the email notification
      * @author amrelzanaty
 
      */
     public function sendMail($userID, $subject, $body) {
         $doctrine = $this->getDoctrine();
         $repo = $doctrine->getRepository('MegasoftEntangleBundle:User');
+        $user = $repo->find($userID); 
+      
+        $acceptmail =$repo->find(array('acceptMailNotifications' => $user->getAcceptMailNotifications())); 
+        
+        $mailrepo =  $doctrine->getRepository("MegasoftEntangleBundle:UserEmail");
+        
+        $useremail = $mailrepo->findBy(array('userId' => $user->getId(), 'deleted' => 0));
+        
+        $maildeleted = $mailrepo->findBy(array('deleted' => $useremail->getDeleted())); 
+        
 
-        $user = $repo->find($userID);
-        $useremail = $doctrine->getRepository("MegasoftEntangleBundle:UserEmail")->findBy(array('userId' => $user->getId(), 'deleted' => 0));
-
-
-        if ($user->getAcceptMailNotifications() == true) {
+        if ($acceptmail == true ) {
             foreach ($useremail as $mail) {
-                if ($mail->getDeleted() == false) {
+                if ($maildeleted == false ) {
                     $message = \Swift_Message::newInstance()
                             ->setSubject($subject)
                             ->addFrom('Notifications-noreply@entangle.io', 'Entangle')
