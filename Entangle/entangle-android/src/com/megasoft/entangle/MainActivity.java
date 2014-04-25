1ac87b0c3e2a685787ea8b60aaa04917a3f556ef
mohamedzayan19 <mohamed.zayan19@gmail.com>
2014-04-25 21:58:28 +0200

    Issue #202 - Add minor fixes to frontend

--- Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
@@ -1,6 +1,5 @@
 package com.megasoft.entangle;
 
-
 import com.megasoft.config.Config;
 
 import android.app.Activity;
@@ -14,15 +13,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-		startActivity(new Intent(this,OfferActivity.class));
-=======
 		Intent intent = new Intent(this, ChangeOfferPriceActivity.class);
 		intent.putExtra("requestId", 5);
 		intent.putExtra("offerId", 5);
 		intent.putExtra(Config.API_SESSION_ID, "5");
 		startActivity(intent);
->>>>>>> 5f748ce5d247c439cb136e1b961130114cb2d4c5
 	}
 
 	@Override
