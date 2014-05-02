4263a48647a5c664eaa3286236f49030e4721217
farghalico <mohamed@farghal.com>
2014-05-02 20:38:32 +0200

    add login template

--- Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MainActivity.java
@@ -15,22 +15,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		startActivity(new Intent(this, HomeActivity.class));
-		//startActivity((new Intent(this,InviteUserActivity.class)).putExtra("com.megasoft.entangle.tangleId", 2));
-
-	}
- 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		//getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		setContentView(R.layout.template_login);
+		getActionBar().hide();
+		
 	}
 	
-	public void moveToRegistration(View view){
-		Intent intent = new Intent(this, CreateTangleActivity.class);
-		startActivity(intent);
-	}
+	
 
 }
