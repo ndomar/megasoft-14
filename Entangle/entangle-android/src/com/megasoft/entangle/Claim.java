bdb27124c8e99383fb5e6d4c0f0ecdd5748ad93f
Salma Amr <salmaumr@gmail.com>
2014-05-03 18:31:41 +0200

    claimform design template

--- Entangle/entangle-android/src/com/megasoft/entangle/Claim.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/Claim.java
@@ -9,9 +9,11 @@ import com.megasoft.requests.PostRequest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Claim extends Activity {
@@ -45,9 +47,16 @@ public class Claim extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.claimform);
+		setContentView(R.layout.activity_claimform);
+		TextView link = (TextView) findViewById(R.id.link);
+		link.setMovementMethod(LinkMovementMethod.getInstance());
+		getActionBar().hide();
 		claimerMail = this.getIntent().getStringExtra("sender");
 		tangleOwenerMail = this.getIntent().getStringExtra("receiver");
+	}
+	
+	public void cancel() { 
+		this.finish();
 	}
 
 	/**
