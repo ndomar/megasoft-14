fb2769cf455c7fd189e95ca320621e2c700e5814
Mansour <mansoursaid001@hotmail.com>
2014-05-04 16:14:51 +0200

    Issue #110 - fixing conflicts

--- Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
@@ -46,6 +46,8 @@ public class CreateTangleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_tangle);
+		this.settings = getSharedPreferences(Config.SETTING, 0);
+		this.sessionId = settings.getString(Config.SESSION_ID, "");
 	}
 
 	@Override
