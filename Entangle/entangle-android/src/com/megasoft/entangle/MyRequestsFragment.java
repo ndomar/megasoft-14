6fa5e83b93bf7b554c5e812cfa7a2dc5f340a567
heba-aamer <heba93aamer@gmail.com>
2014-05-05 00:17:04 +0200

    Issue #371 - send the request and

--- Entangle/entangle-android/src/com/megasoft/entangle/MyRequestsFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/MyRequestsFragment.java
@@ -68,5 +68,80 @@ public class MyRequestsFragment extends Fragment {
 
 	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater
+				.inflate(R.layout.activity_tangle, container, false);
 
+		tangleId = getArguments().getInt("tangleId");
+		tangleName = getArguments().getString("tangleName");
+		
+		sendRequest(rootResource + "/tangle/" + tangleId + "/userRequests");
+		return view;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		this.activity = (FragmentActivity) activity;
+		super.onAttach(activity);
+	}
+	
+	private void setTheLayout(String res) {
+		
+	}
+	
+	/**
+	 * This method is used to send a get request to get requests of certain user
+	 * 
+	 * @param url
+	 *            , is the URL to which the request is going to be sent
+	 */
+	public void sendRequest(final String url) {
+		sessionId = activity.getSharedPreferences(Config.SETTING, 0).getString(
+				Config.SESSION_ID, "");
+		GetRequest getStream = new GetRequest(url) {
+			protected void onPostExecute(String res) {
+				if (!this.hasError() && res != null) {
+					LinearLayout layout = (LinearLayout) activity
+							.findViewById(R.id.streamLayout);
+					layout.removeAllViews();
+					setTheLayout(res);
+				} else {
+					Toast.makeText(activity.getBaseContext(),
+							"Sorry, There is a problem in loading the stream",
+							Toast.LENGTH_LONG).show();
+				}
+			}
+		};
+		getStream.addHeader(Config.API_SESSION_ID, getSessionId());
+		getStream.execute();
+	}
+
+	/**
+	 * This is a getter method used to get the tangle name
+	 * 
+	 * @return tangle name
+	 */
+	private String getTangleName() {
+		return tangleName;
+	}
+
+	/**
+	 * This is a getter method used to get the session id of the user
+	 * 
+	 * @return session id
+	 */
+	private String getSessionId() {
+		return sessionId;
+	}
+
+	/**
+	 * This is a getter method used to get the tangle id
+	 * 
+	 * @return tangle id
+	 */
+	private int getTangleId() {
+		return tangleId;
+	}
 }
