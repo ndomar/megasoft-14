c952ddf793f589cf70cbbf474226d01ed231db7f
Eslamunto <eslam.shala@gmail.com>
2014-05-05 12:40:11 +0200

    Issue#372 - Remove image upload

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/SiteController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/SiteController.php
@@ -93,25 +93,25 @@ class SiteController extends Controller
                 $userEmail->setEmail($email);
                 $user->setUserBio($userBio);
                 $user->setVerified(FALSE);
-                $image = $request->files->get('img');
-                if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
-                    if ($image->getSize() < 4194304) { //if image size is less that 4MB
-                        $originalName = $image->getClientOriginalName();
-                        $nameArray = explode('.', $originalName);
-
-                        $fileType = $nameArray[sizeof($nameArray) - 1];
-                        $validFileTypes = array('jpg', 'jpeg', 'bmp',
-                            'png');
-
-                        if (in_array(strtolower($fileType), $validFileTypes)) {
-                            $kernel = $this->get('kernel');
-                            $filename = substr(md5(time()), 0, 10) . $this->generate(5);
-                            $filepath = $kernel->getRootDir() . '/../web/images/profilePictures/' . $filename . '.' . $fileType;
-                            move_uploaded_file($image, $filepath);
-                            $user->setPhoto($filename . '.' .$fileType);
-                        }
-                    }
-                }
+                //$image = $request->files->get('img');
+//                if (($image instanceof UploadedFile) && ($image->getError() == '0')) {
+//                    if ($image->getSize() < 4194304) { //if image size is less that 4MB
+//                        $originalName = $image->getClientOriginalName();
+//                        $nameArray = explode('.', $originalName);
+//
+//                        $fileType = $nameArray[sizeof($nameArray) - 1];
+//                        $validFileTypes = array('jpg', 'jpeg', 'bmp',
+//                            'png');
+//
+//                        if (in_array(strtolower($fileType), $validFileTypes)) {
+//                            $kernel = $this->get('kernel');
+//                            $filename = substr(md5(time()), 0, 10) . $this->generate(5);
+//                            $filepath = $kernel->getRootDir() . '/../web/images/profilePictures/' . $filename . '.' . $fileType;
+//                            move_uploaded_file($image, $filepath);
+//                            $user->setPhoto($filename . '.' .$fileType);
+//                        }
+//                    }
+//                }
                 $entityManager = $this->getDoctrine()->getEntityManager();
                 $entityManager->persist($user);
                 $entityManager->persist($userEmail);
