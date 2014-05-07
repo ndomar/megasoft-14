c5ae4ce7b27c1543ed3968d2366ced1e83496842
KareemWahby <kareem.wahby@gmail.com>
2014-05-07 16:23:12 +0200

    Issue #400 - fixed the Backend

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/PasswordForgetController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/PasswordForgetController.php
@@ -9,59 +9,57 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\False;
 
-class PasswordReset extends Controller{
+class PasswordForgetController extends Controller{
 
     /**
-     * this function checks that the data of the user is valid so a new password could be generated
+     * this function validates that the user credentials are correct
      * @param $name
      * @param $email
-     * @param $dob
-     * @return bool true if the security checks are passed false otherwise
+     * @return int
      * @author KareemWahby
      */
-    private function securityCheck($name,$email,$dob) {
-        if ($name == null ||$email == null || $dob == null) {
-            return false;
+    private function securityCheck($name,$email) {
+        if ($name == null ||$email == null) {
+            return -1;
         }
         $doctrine = $this->getDoctrine();
         $UserRepo = $doctrine->getRepository('MegasoftEntangleBundle:User');
         $emailRepo= $doctrine->getRepository('MegasoftEntangleBundle:UserEmail');
-        $user = $UserRepo->findOneBy(array('name' => $name,'birthDate' => $dob));
-        if ($user == null) {
-            return false;
+        $user = $UserRepo->findOneBy(array('name' => $name));
+        $userId= $user-> getId();
+        $userEmail= $emailRepo->findOneBy(array('email' => $email));
+        $userIdByMail= $userEmail-> getUserID();
+        if($userId != $userIdByMail){
+           return -1;
         }
-        $userId= $user-> getUserId();
-        $userEmail= $emailRepo->findOneBy(array('userId' => $userId));
-        $mail= $userEmail->getEmail();
-        if($email != $mail){
-           return false;
-        }
-        return true;
+        return $userId;
     }
 
     /**
-     * this is the endpoint for reseting the password of the user
+     * this function is the endpiont that returns the user password if he forgets it
      * @param Request $request
      * @return JsonResponse
      * @author KareemWahby
      */
-    public function retrievePassword(Request $request){
-        $data = $request->getContent($request,true);
+    public function retrievePasswordAction(Request $request){
+        $data = json_decode($request->getContent(),true);
         $name = $data['name'];
         $email= $data['email'];
-        $dob= $data['birthDate'];
         $response= new JsonResponse();
-        if(!$this->securityCheck($name,$email,$dob)){
+        $id=$this->securityCheck($name,$email);
+        if($id == -1){
             $response->sendContent("Bad Request");
             $response->setStatusCode(400);
             return $response;
+        }else{
+            $doctrine = $this->getDoctrine();
+            $UserRepo = $doctrine->getRepository('MegasoftEntangleBundle:User');
+            $user = $UserRepo->findOneBy(array('id' => $id));
+            $password=$user->getPassword();
+            $response->setStatusCode(200);
+            $response->setContent(json_encode(array('password'=> $password)));
+            return $response;
         }
-        $doctrine = $this->getDoctrine();
-        $UserRepo = $doctrine->getRepository('MegasoftEntangleBundle:User');
-        $user = $UserRepo->findOneBy(array('name' => $name,'birthDate' => $dob));
-        $password=$user->getPassword();
-        $response->setStatusCode(200);
-        $response->setData(array('password'=> $password));
-        return $response;
+
     }
 } 
