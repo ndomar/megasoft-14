9ca8d802498de1c4854847e2d46efa20625fb522
heba-aamer <heba93aamer@gmail.com>
2014-05-09 12:15:58 +0200

    Issue #371 - creating open request fragment

---
+++ Entangle/entangle-android/src/com/megasoft/entangle/OpenRequestFragment.java
@@ -0,0 +1,18 @@
+package com.megasoft.entangle;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OpenRequestFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_open_request,
+				container, false);
+		return rootView;
+	}
+
+}
