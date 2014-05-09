d285e67524e8be398948835b40a95099164adc6b
heba-aamer <heba93aamer@gmail.com>
2014-05-09 12:31:16 +0200

    Issue #371 - creating frozen and closed requests classes

---
+++ Entangle/entangle-android/src/com/megasoft/entangle/ClosedRequestFragment.java
@@ -0,0 +1,7 @@
+package com.megasoft.entangle;
+
+import android.support.v4.app.Fragment;
+
+public class ClosedRequestFragment extends Fragment {
+
+}
---
+++ Entangle/entangle-android/src/com/megasoft/entangle/FrozenRequestFragment.java
@@ -0,0 +1,7 @@
+package com.megasoft.entangle;
+
+import android.support.v4.app.Fragment;
+
+public class FrozenRequestFragment extends Fragment {
+
+}
