0119887e5543303bf4ff39ce991b94a6fc46aa3a
heba-aamer <heba93aamer@gmail.com>
2014-05-10 01:50:34 +0200

    Issue #388 - adding helper methods

--- Entangle/entangle-android/src/com/megasoft/entangle/EntryOfferFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/EntryOfferFragment.java
@@ -24,15 +24,15 @@ public class EntryOfferFragment extends Fragment {
 			Bundle savedInstanceState) {
 		view = inflater
 				.inflate(R.layout.template_offer_entry, container, false);
+		setAttributes();
 		view.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getActivity(), OfferActivity.class);
-				intent.putExtra("offerID", offerId);
+				intent.putExtra("offerID", getOfferId());
 				startActivity(intent);
 			}
 		});
-		setAttributes();
 		return view;
 	}
 
@@ -43,13 +43,33 @@ public class EntryOfferFragment extends Fragment {
 	 */
 	private void setAttributes() {
 		Bundle args = getArguments();
-		offerId = args.getInt("offerId");
+		setOfferId(args.getInt("offerId"));
 		((TextView) view.findViewById(R.id.offerPrice)).setText(args
 				.getString("requestedPrice"));
 		((TextView) view.findViewById(R.id.offerDescription)).setText(args
 				.getString("description"));
 		((TextView) view.findViewById(R.id.offererName)).setText(args
 				.getString("offerer"));
-		this.offerId = args.getInt("offerId");
+	}
+
+	/**
+	 * This a getter method to get the id of the offer
+	 * 
+	 * @return offerID
+	 * @author HebaAamer
+	 */
+	public int getOfferId() {
+		return offerId;
+	}
+
+	/**
+	 * This is a setter method to set the id of the offer
+	 * 
+	 * @param id
+	 *            , id of the offer
+	 * @author HebaAamer
+	 */
+	public void setOfferId(int id) {
+		this.offerId = id;
 	}
 }
