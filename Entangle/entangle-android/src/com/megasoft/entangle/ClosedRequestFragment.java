5ca8a8966787e40568728f7b8021113605a0beec
heba-aamer <heba93aamer@gmail.com>
2014-05-09 13:40:06 +0200

    Issue #371 - adding the reopen button to the closed request layout

--- Entangle/entangle-android/src/com/megasoft/entangle/ClosedRequestFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ClosedRequestFragment.java
@@ -1,7 +1,21 @@
 package com.megasoft.entangle;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ClosedRequestFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_closed_request,
+				container, false);
+		return rootView;
+	}
 
+	public void reOpenRequest() {
+
+	}
 }
