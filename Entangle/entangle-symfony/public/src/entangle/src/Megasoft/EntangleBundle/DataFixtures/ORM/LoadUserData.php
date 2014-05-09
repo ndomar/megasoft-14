4d3fafe6ce8082216f408188028fd437a1034b70
OmarElAzazy <oa12gb@gmail.com>
2014-05-10 00:30:18 +0200

    Issue #430 - add user fixtures

---
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Megasoft\EntangleBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Megasoft\EntangleBundle\Entity\User;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('sampleUser');
+        $user->setPassword('samplePassword');
+        $user->setVerified(true);
+        
+        $manager->persist($user);
+        $manager->flush();
+        
+        $this->addReference('sampleUser', $user);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
