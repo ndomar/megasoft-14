d3a7fc809cc593370f2042ca029af618502cd701
heba-aamer <heba93aamer@gmail.com>
2014-05-09 17:27:35 +0200

    Issue #371 - setting the buttons in the 2 classes

--- Entangle/entangle-android/src/com/megasoft/entangle/ClosedRequestFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/ClosedRequestFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ClosedRequestFragment extends StreamRequestFragment {
+	Button reOpenRequest;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_closed_request, container,
 				false);
 		setAttributes();
+		reOpenRequest = (Button) view.findViewById(R.id.reopenRequestButton);
+		reOpenRequest.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				reOpenRequest();
+			}
+		});
 		return view;
 	}
 
--- Entangle/entangle-android/src/com/megasoft/entangle/OpenRequestFragment.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/OpenRequestFragment.java
@@ -4,14 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class OpenRequestFragment extends StreamRequestFragment {
+	Button deleteRequest;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_open_request, container,
 				false);
 		setAttributes();
+		deleteRequest = (Button) view.findViewById(R.id.deleteRequestButton);
+		deleteRequest.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				deleteRequest();
+
+			}
+		});
 		return view;
 	}
 
