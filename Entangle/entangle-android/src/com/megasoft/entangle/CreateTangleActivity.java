093179abded316890ddcd79508a0aae2ebb4a6c5
Mansour <mansoursaid001@hotmail.com>
2014-05-09 03:52:35 +0200

    Issue #110 - Code cleanup

--- Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
@@ -9,18 +9,11 @@ import com.megasoft.config.Config;
 import com.megasoft.requests.PostRequest;
 
 import android.util.Base64;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -29,16 +22,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateTangleActivity extends Activity {
 
-	private static final int REQUEST_CODE = 1;
 	public static final int BUTTON_POSITIVE = 0xffffffff;
 	public static final int GREEN = 0xff00ff00;
 	public static final int RED = 0xffff0000;
@@ -94,76 +84,11 @@ public class CreateTangleActivity extends Activity {
 		return true;
 	}
 
-	/**
-	 * Redirects to the gallery when clicking choose icon button.
-	 * 
-	 * @param View
-	 *            view
-	 * @author Mansour
-	 */
-	public void chooseIcon(View view) {
-		goToGallery();
-	}
-
-	/**
-	 * Goes to the gallery to pick an image from there.
-	 * 
-	 * @author Mansour
-	 */
-	public void goToGallery() {
-		startActivityForResult(new Intent(Intent.ACTION_PICK,
-				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI),
-				REQUEST_CODE);
-	}
-
-	/**
-	 * Waits for the image picked from the gallery and encodes it to a String.
-	 * 
-	 * @param int requestCode
-	 * @param int resultCode
-	 * @param Intent
-	 *            data
-	 * @author Mansour
-	 */
-	@SuppressLint("NewApi")
-//	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//		super.onActivityResult(requestCode, resultCode, data);
-//		if (resultCode == RESULT_OK && requestCode == REQUEST_CODE
-//				&& null != data) {
-//			Bitmap bitmap = getPhotoPath(data.getData());
-//			Button tangleIcon = (Button) findViewById(R.id.tangleIcon);
-//			tangleIcon
-//					.setBackground(new BitmapDrawable(getResources(), bitmap));
-//			encodeToBase64(bitmap);
-//		}
-//	}
-
 	public void encodeToBase64(Bitmap bitmap) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
 		byte[] byteArray = baos.toByteArray();
 		encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
-	}
-
-	/**
-	 * Get the path of the photo the user picked from the gallery and returns
-	 * the image as a bitmap.
-	 * 
-	 * @param uri
-	 * @return Bitmap
-	 * @author Mansour
-	 */
-	public Bitmap getPhotoPath(Uri uri) {
-		String[] projection = { android.provider.MediaStore.Images.Media.DATA };
-		Cursor cursor = getContentResolver().query(uri, projection, null, null,
-				null);
-		int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
-		cursor.moveToFirst();
-		String filePath = cursor.getString(columnIndex);
-		cursor.close();
-		Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-
-		return bitmap;
 	}
 
 	/**
@@ -234,56 +159,6 @@ public class CreateTangleActivity extends Activity {
 	}
 
 	/**
-	 * Shows a message dialogue to the user when called showing the message that
-	 * is called with.
-	 * 
-	 * @param message
-	 * @author Mansour
-	 */
-	public void showMessage(String message) {
-		AlertDialog ad = new AlertDialog.Builder(this).create();
-		ad.setCancelable(false);
-		ad.setMessage(message);
-		ad.setButton(BUTTON_POSITIVE, "OK",
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
-		ad.show();
-	}
-
-	/**
-	 * Resets a color of a text in a text field to black.
-	 * 
-	 * @param id
-	 * @author Mansour
-	 */
-	public void resetColor(int id) {
-		TextView textView = (TextView) findViewById(id);
-		textView.setTextColor(BLACK);
-	}
-
-	/**
-	 * Changes the color of a text inside a text field to green or red according
-	 * to its availability.
-	 * 
-	 * @param available
-	 * @param id
-	 * @author Mansour
-	 */
-	public void insertAvailability(boolean available, int id) {
-
-		TextView textView = (TextView) findViewById(id);
-		if (available == true) {
-			textView.setTextColor(GREEN);
-		} else {
-			textView.setTextColor(RED);
-		}
-	}
-
-	/**
 	 * Shows a dialogue informing that the tangle is created and redirecting to
 	 * homepage when pressing OK.
 	 * 
@@ -292,25 +167,6 @@ public class CreateTangleActivity extends Activity {
 	public void goToHomePage() {
 		Toast.makeText(getApplicationContext(), "Your Tangle Has Been Created",
 				Toast.LENGTH_LONG).show();
-		goToHomeHelper();
-	}
-
-	/**
-	 * Redirects the user to the home page.
-	 * 
-	 * @author Mansour
-	 */
-	public void goToHomeHelper() {
-		this.finish();
-	}
-
-	/**
-	 * Returns to the parent activity when pressing the cancel button.
-	 * 
-	 * @param view
-	 * @author Mansour
-	 */
-	public void cancelRedirect(View view) {
 		this.finish();
 	}
 
