194130698a834b22f053e5ae0968324f5f4072bf
heba-aamer <heba93aamer@gmail.com>
2014-05-10 01:34:35 +0200

    Issue #388 - adding EntryOfferFragment file

---
+++ Entangle/entangle-android/src/com/megasoft/entangle/EntryOfferFragment.java
@@ -0,0 +1,18 @@
+package com.megasoft.entangle;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class EntryOfferFragment extends Fragment {
+	/**
+	 * This is the id of the offer
+	 */
+	private int offerId;
+
+	/**
+	 * This is the view of the fragment
+	 */
+	private View view;
+
+	
+}
