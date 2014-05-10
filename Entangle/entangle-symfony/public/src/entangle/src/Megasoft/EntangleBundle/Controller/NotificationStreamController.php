4916255c2832f7a5336baebad09e3b6b07fe1120
mAyman93 <moh.ayman.93@gmail.com>
2014-05-11 01:49:50 +0200

    Issue #424 editing the controller

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/NotificationStreamController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/NotificationStreamController.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * Gets all the notifications of a user
 * @author : Mohamed Ayman
 *
 */
@@ -13,25 +14,72 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NotificationStreamController extends Controller
 {
-	public function getNotificationAction($userId)
-	{
-		$notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:Notification');
-		$notifications = $notificationRepo->findBy(array('userId' => $userId));
-		if(count($notifications) == 0)
-		{
-			return new Response("No notifications found" , 400);
-		}
+    //The array that will have all the notifications
+    private $arr;
+
+    /**
+     * @param $userId
+     * @return The name of the user
+     * @author : Mohamed Ayman
+     */
+    public function getName($userId)
+    {
+        $userRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:User');
+        $user = $userRepo->findOneById($userId);
+        return $user->getName();
+    }
+
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getClaimNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:NewClaimNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
         $count = count($notifications);
-        $arr = array();
         for($i = 0; $i < $count ; $i++)
         {
             $notification = $notifications[$i];
-            array_push($arr , $notification->getId());
+            $claimerId = $notification->getClaim()->getClaimerId();
+            $claimerName = $this->getName($claimerId);
+            $description = $claimerName." has claimed you!";
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "claim=".$notification->getClaimId());
+            array_push($this->arr , $array);
         }
+    }
 
-		$response = new JsonResponse();
-		$response->setdata($arr);
-		$response->setStatusCode(200);
-		return $response;
-	}
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getPriceChangeNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:PriceChangeNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
+        $count = count($notifications);
+        for($i = 0; $i < $count ; $i++)
+        {
+            $notification = $notifications[$i];
+            $offererId = $notification->getOffer()->getUserId();
+            $offererName = $this->getName($offererId);
+            $description = $offererName." has changed the price to ".$notification->getNewPrice();
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "offer=".$notification->getOfferId());
+            array_push($this->arr , $array);
+        }
+    }
+
+
 }
