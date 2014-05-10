a7d23fa67b2cced2daab3dca1bde5045b079b286
mAyman93 <moh.ayman.93@gmail.com>
2014-05-11 01:50:51 +0200

    Issue #424 editing the controller again

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/NotificationStreamController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/NotificationStreamController.php
@@ -81,5 +81,109 @@ class NotificationStreamController extends Controller
         }
     }
 
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getNewOfferNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:NewOfferNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
+        $count = count($notifications);
+        for($i = 0; $i < $count ; $i++)
+        {
+            $notification = $notifications[$i];
+            $offererId = $notification->getOffer()->getUserId();
+            $offererName = $this->getName($offererId);
+            $description = "You got a new offer from ".$offererName;
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "offer=".$notification->getOfferId());
+            array_push($this->arr , $array);
+        }
+    }
+
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getReopenRequestNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:ReopenRequestNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
+        $count = count($notifications);
+        for($i = 0; $i < $count ; $i++)
+        {
+            $notification = $notifications[$i];
+            $otherUserId = $notification->getRequest()->getUserId();
+            $name = $this->getName($otherUserId);
+            $description = $name." has re-opened his request!";
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "request=".$notification->getRequestId());
+            array_push($this->arr , $array);
+        }
+    }
+
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getOfferChosenNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:OfferChosenNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
+        $count = count($notifications);
+        for($i = 0; $i < $count ; $i++)
+        {
+            $notification = $notifications[$i];
+            $otherUserId = $notification->getOffer()->getUserId();
+            $name = $this->getName($otherUserId);
+            $description = $name." has chosen your offer!";
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "offer=".$notification->getOfferId());
+            array_push($this->arr , $array);
+        }
+    }
+
+    /**
+     * @param $userId
+     * Adds the Notifications into the array
+     * @author : Mohamed Ayman
+     */
+    public function getRequestDeletedNotifications($userId)
+    {
+        $notificationRepo = $this->getDoctrine()->getRepository('MegasoftEntangleBundle:RequestDeletedNotification');
+        $notifications = $notificationRepo->findBy(array('userId' => $userId));
+        if(count($notifications) == 0)
+        {
+            return;
+        }
+        $count = count($notifications);
+        for($i = 0; $i < $count ; $i++)
+        {
+            $notification = $notifications[$i];
+            $otherUserId = $notification->getRequest()->getUserId();
+            $name = $this->getName($otherUserId);
+            $description = $name." has deleted his request!";
+            $array = array($description , $notification->getId() , $notification->getCreated() ,
+                $notification->getSeen() , "request=".$notification->getRequestId());
+            array_push($this->arr , $array);
+        }
+    }
+
 
 }
