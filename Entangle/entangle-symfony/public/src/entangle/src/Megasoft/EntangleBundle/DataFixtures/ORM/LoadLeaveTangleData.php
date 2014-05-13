d9bed168aee2ef0ab9239aa58899830684f6ffa1
heba-aamer <heba93aamer@gmail.com>
2014-05-14 00:36:03 +0200

    Issue #164 - adding createTangle and createUserTangle functions in the fixtures

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadLeaveTangleData.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadLeaveTangleData.php
@@ -25,6 +25,12 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         $this->createSession($manager, 'userMohamed', 'userMohamed', true, 2);
         $this->createSession($manager, 'userAly', 'userAly', false, 3);
         
+        $this->createTangle($manager);
+        
+        $this->createUserTangle($manager, 'userAhmad', true);
+        $this->createUserTangle($manager, 'userMohamed', false);
+        $this->createUserTangle($manager, 'userAly', false);
+        
         $manager->flush();
     }
 
@@ -57,5 +63,25 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         
         $manager->persist($session);
         $this->addReference('session' . "$reference", $session);
+    }
+    
+    private function createTangle(ObjectManager $manager) {
+        $tangle = new Tangle();
+        $tangle->setName('Tangle');
+        $tangle->setDescription('Sample tangle');
+        
+        $manager->persist($tangle);
+        $this->addReference('tangle', $tangle);
+    }
+    
+    private function createUserTangle(ObjectManager $manager, $userReference, $isOwner) {
+        $userTangle = new UserTangle();
+        $userTangle->setUser($this->getReference("$userReference"));
+        $userTangle->setTangle($this->getReference('tangle'));
+        $userTangle->setTangleOwner($isOwner);
+        $userTangle->setCredit(0);
+        
+        $manager->persist($userTangle);
+        $this->addReference('userTangle' . "$userReference", $userTangle);
     }
 }
