0e148bc9467455b31c3fcdc54c34cd20c05712a8
naderdavid <emailandmail@yahoo.com>
2014-05-13 19:54:12 +0200

    Issue #401 - adding action to handle the image sent request

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/UserController.php
@@ -449,5 +449,63 @@ class UserController extends Controller {
             return $response;
         }
     }
+    public function postImageAction(Request2 $request)
+    {
+        $sessionId = $request->headers->get('X-SESSION-ID');
+
+        if ($sessionId == null) {
+            return new Response('Bad Request', 400);
+        }
+
+        $doctrine = $this->getDoctrine();
+
+        $sessionRepo = $doctrine->getRepository('MegasoftEntangleBundle:Session');
+        $session = $sessionRepo->findOneBy(array('sessionId' => $sessionId));
+        if ($session == null || $session->getExpired()) {
+            return new Response('Bad Request', 400);
+        }
+
+        $jsonString = $request->getContent();
+
+        if ($jsonString == null) {
+            return new Response('Bad Request', 400);
+        }
+
+        $json = json_decode($jsonString, true);
+        $picData = $json['Picture'];
+
+        if ($picData == null) {
+            return new Response('Bad Request', 400);
+        }
+
+        try {
+            $picUrl = $this->savePic($picData);
+        } catch (Exception $e) {
+            return new Response('Internal Server Error', 500);
+        }
+        
+        $userId = $session->getUserId();
+        $userRepo = $doctrine->getRepository('MegasoftEntangleBundle:User');
+        $user = $userRepo->findOneById(array('id' => $userId));
+        $user->setPhoto($picUrl);
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+        return new Response('Ok',200);
+        
+    }
+    private function savePic($picData)
+    {
+        $decodedPic = base64_decode($picData);
+        $pic = imagecreatefromstring($decodedPic);
+
+        $picFileName = 'request' . "$requestId" . '.png';
+        $kernel = $this->get('kernel');
+        $path = $kernel->getRootDir() . '/../web/bundles/megasoftentangle/images/profile/pictures/';
+
+        $outputFilePath = $path . $picFileName;
+        imagepng($pic, $outputFilePath, 9);
+        imagedestroy($pic);
+        return 'http://10.11.12.13/entangle/web/bundles/megasoftentangle/images/profile/pictures/' . $iconFileName;
+    }
 
 }
