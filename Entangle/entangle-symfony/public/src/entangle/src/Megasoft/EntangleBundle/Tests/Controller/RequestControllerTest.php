9cd8fbaf54d6f26c4e4e3e10f8e9d84fd9aba670
OmarElAzazy <oa12gb@gmail.com>
2014-05-14 06:08:33 +0200

    Issue #453 - Add already deleted request test case

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
@@ -108,4 +108,24 @@ class RequestControllerTest extends EntangleTestCase
         $this->assertEquals(401, $client->getResponse()->getStatusCode(),
             'Check for status code of unauthorized for request of another user');
     }
+
+    /*
+     * Test case testing sending a request to delete request end point
+     * with id of already deleted request
+     * @author OmarElAzazy
+     */
+    public function testDeleteAction_DeletedRequest(){
+        $this->addFixture(new LoadDeleteRequestData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('DELETE',
+            '/request/3',
+            array(),
+            array(),
+            array('HTTP_X_SESSION_ID'=>'sessionUser1'));
+
+        $this->assertEquals(400, $client->getResponse()->getStatusCode(),
+            'Check for status code of bad request for already deleted request');
+    }
 }
