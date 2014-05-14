ac18e027e91f2010778f3d7fec927b478f0ca245
Mansour <mansoursaid001@hotmail.com>
2014-05-14 19:26:18 +0200

    Issue #110 - Cleaning Code

--- Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/CreateTangleActivity.java
@@ -128,7 +128,7 @@ public class CreateTangleActivity extends Activity {
 				} else {
 					if (this.getStatusCode() != 201) {
 						Toast.makeText(getApplicationContext(),
-								"Try Again Later"+this.getErrorMessage(), Toast.LENGTH_LONG).show();
+								"Try Again Later", Toast.LENGTH_LONG).show();
 					} else {
 						goToHomePage();
 					}
