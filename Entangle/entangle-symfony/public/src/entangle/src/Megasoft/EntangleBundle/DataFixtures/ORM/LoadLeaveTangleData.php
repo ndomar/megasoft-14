c5fc786571628a66a60f3f2c6580dbc38d6e4556
heba-aamer <heba93aamer@gmail.com>
2014-05-14 20:12:58 +0200

    Issue #164 - finalizing the fixture

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadLeaveTangleData.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadLeaveTangleData.php
@@ -23,6 +23,7 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         $this->makeUserTangles($manager);
         $this->makeRequests($manager);
         $this->makeOffers($manager);
+        $this->makeMessages($manager);
         $this->makeTransactions($manager);
         $this->makeClaims($manager);
                 
@@ -47,17 +48,16 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
     }
     
     
-    private function createSession(ObjectManager $manager, $reference, $sessionId, $expired, $regId) {
+    private function createSession(ObjectManager $manager, $userReference, $sessionId, $expired, $regId) {
         $session = new Session();
-        $session->setUser($this->getReference("$reference"));
+        $session->setUser($this->getReference("$userReference"));
         $session->setSessionId("$sessionId");
         $session->setExpired($expired);
         $session->setCreated(new DateTime('now'));
         $session->setDeviceType('Samsung S4');
-        $session->setRegId($regId);
+        $session->setRegId("$regId");
         
         $manager->persist($session);
-        $this->addReference('session' . "$reference", $session);
     }
     
     private function createUserEmail(ObjectManager $manager, $userReference) {
@@ -85,7 +85,6 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         $userTangle->setCredit($credit);
         
         $manager->persist($userTangle);
-        $this->addReference('userTangle_' . "$userReference", $userTangle);
     }
     
     private function createRequest(ObjectManager $manager, $userReference, $description, $requestNumber, $requestStatus) {
@@ -118,10 +117,9 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         $transaction->setFinalPrice(30);
         
         $manager->persist($transaction);
-        $this->addReference('transaction', $transaction);
     }
     
-    private function createClaim(ObjectManager $manager, $offerReference, $userReference, $claimNumber) {
+    private function createClaim(ObjectManager $manager, $offerReference, $userReference) {
         $claim = new Claim();
         $claim->setClaimer($this->getReference("$userReference"));
         $claim->setTangle('tangle');
@@ -129,7 +127,16 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
         $claim->setCreated(new DateTime('now'));
         
         $manager->persist($claim);
-        $this->addReference('claim' . "$claimNumber", $claim);
+    }
+    
+    private function createMessage(ObjectManager $manager, $messageBody, $userReference, $offerReference){
+        $message = new Message();
+        $message->setOffer($this->getReference("$offerReference"));
+        $message->setSender($this->getReference("$userReference"));
+        $message->setDate(new DateTime('now'));
+        $message->setBody($messageBody);
+        
+        $manager->persist($message);
     }
     
     private function makeUsers(ObjectManager $manager){
@@ -145,10 +152,10 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
     }
     
     private function makeSessions(ObjectManager $manager){
-        $this->createSession($manager, 'userAhmad', 'userAhmad', false, 1);
-        $this->createSession($manager, 'userMohamed', 'userMohamed', true, 2);
-        $this->createSession($manager, 'userAly', 'userAly', false, 3);
-        $this->createSession($manager, 'userMazen', 'userMazen', false, 4);
+        $this->createSession($manager, 'userAhmad', 'userAhmad', false, '1');
+        $this->createSession($manager, 'userMohamed', 'userMohamed', true, '2');
+        $this->createSession($manager, 'userAly', 'userAly', false, '3');
+        $this->createSession($manager, 'userMazen', 'userMazen', false, '4');
     }
     
     private function makeTangles(ObjectManager $manager){
@@ -162,28 +169,39 @@ class LoadLeaveTangleData extends AbstractFixture implements OrderedFixtureInter
     }
     
     private function makeRequests(ObjectManager $manager){
-        $this->createRequest($manager, 'userMohamed', 'i want to buy a car', 1);
-        $this->createRequest($manager, 'userMohamed', 'i want to travel to London', 2);
-        $this->createRequest($manager, 'userMohamed', 'i want to go to the doctor', 3);
-        $this->createRequest($manager, 'userAly', 'i want to buy a book', 4);
-        $this->createRequest($manager, 'userMohamed', 'i want to have a reminder software', 5);
-        $this->createRequest($manager, 'userAly', 'i want to have a ride tomorrow', 6);
+        $this->createRequest($manager, 'userMohamed', 'i want to buy a car', 1, 1);
+        $this->createRequest($manager, 'userMohamed', 'i want to travel to London', 2, 0);
+        $this->createRequest($manager, 'userMohamed', 'i want to go to the doctor', 3, 2);
+        $this->createRequest($manager, 'userAly', 'i want to buy a book', 4, 0);
+        $this->createRequest($manager, 'userAly', 'i want to have a reminder software', 5, 2);
+        $this->createRequest($manager, 'userAly', 'i want to have a ride tomorrow', 6, 1);
     }
     
     private function makeOffers(ObjectManager $manager){
-        $this->createOffer($manager, 'userAly', 'i can help', 1, 'request1');
-        $this->createOffer($manager, 'userAly', 'i want to help', 2, 'request2');
-        $this->createOffer($manager, 'userAly', 'i can do this for you', 3, 'request3');
-        $this->createOffer($manager, 'userAly', 'i will help you', 4, 'request4');
+        $this->createOffer($manager, 'userAly', 'i can help', 1, 'request1', 1);
+        $this->createOffer($manager, 'userAly', 'i want to help', 2, 'request2', 0);
+        $this->createOffer($manager, 'userAly', 'i can do this for you', 3, 'request3', 2);
+        $this->createOffer($manager, 'userMohamed', 'i will help you', 4, 'request4', 3);
+        $this->createOffer($manager, 'userMohamed', 'i can help you', 5, 'request5', 2);
+        $this->createOffer($manager, 'userMohamed', 'this is easy', 6, 'request6', 1);
+    }
+    
+    private function makeMessages(ObjectManager $manager){
+        $this->createMessage($manager, "hi1", 'userMohamed', 'offer1');
+        $this->createMessage($manager, "hi2", 'userAly', 'offer1');
+        $this->createMessage($manager, "hi3", 'userMohamed', 'offer1');
+        $this->createMessage($manager, "hi4", 'userAly', 'offer2');
+        $this->createMessage($manager, "hi5", 'userAly', 'offer2');
     }
     
     private function makeTransactions(ObjectManager $manager){
         $this->createTransaction($manager, 'offer1');
+        $this->createTransaction($manager, 'offer6');
     }
     
     private function makeClaims(ObjectManager $manager){
-        $this->createClaim($manager, 'offer2', 'userAly', 1);
-        $this->createClaim($manager, 'offer2', 'userMohamed', 2);
-        $this->createClaim($manager, 'offer2', 'userAly', 3);         
+        $this->createClaim($manager, 'offer3', 'userAly');
+        $this->createClaim($manager, 'offer3', 'userMohamed');
+        $this->createClaim($manager, 'offer5', 'userAly');         
     }
 }
