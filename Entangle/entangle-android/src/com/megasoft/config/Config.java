7e6188624da8f3fffed6d6c2c31f74d371431095
Salma Amr <salmaumr@gmail.com>
2014-05-14 22:31:19 +0200

    Issue #393 - Add files

--- Entangle/entangle-android/src/com/megasoft/config/Config.java
+++ Entangle/entangle-android/src/com/megasoft/config/Config.java
@@ -2,7 +2,7 @@ package com.megasoft.config;
 
 public class Config {
 
-	public static final String API_BASE_URL = "http://entangle.io/app_dev.php";
+	public static final String API_BASE_URL = "http://192.168.1.115:9001/entangle/web/app_dev.php";
 	public static final String API_BASE_URL_SERVER = API_BASE_URL;
 	public static final String API_SESSION_ID = "X-SESSION-ID";
 	public static final String SETTING = "SETTINGS";
