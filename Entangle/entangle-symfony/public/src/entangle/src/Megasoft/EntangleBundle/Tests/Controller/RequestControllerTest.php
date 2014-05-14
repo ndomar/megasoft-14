84474e6629f822947d22884fdd1e6858d06090ee
OmarElAzazy <oa12gb@gmail.com>
2014-05-14 05:55:15 +0200

    Issue #453 - Add bad session id test case

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
@@ -25,6 +25,27 @@ class RequestControllerTest extends EntangleTestCase
         $client->request('DELETE',
             '/request/1');
 
-        $this->assertEquals(400, $client->getResponse()->getStatusCode(), 'Check for status code of bad request');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode(),
+            'Check for status code of bad request for no session id');
+    }
+
+    /*
+     * Test case testing sending a request to delete request end point
+     * with bad session id
+     * @author OmarElAzazy
+     */
+    public function testDeleteAction_BadSessionId(){
+        $this->addFixture(new LoadDeleteRequestData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('DELETE',
+            '/request/1',
+            array(),
+            array(),
+            array('HTTP_X_SESSION_ID'=>'badSessionId'));
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode(),
+            'Check for status code of bad request for bad session id');
     }
 }
