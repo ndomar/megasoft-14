02607cdc46e8a978289e33ebbfc8f805cdc48f05
MahmoudGamalEid <mahmoudgamaleid@gmail.com>
2014-05-14 20:08:06 +0200

    Issue #404 - Edit VerificationController

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/VerificationController.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Controller/VerificationController.php
@@ -28,7 +28,7 @@ class VerificationController extends Controller {
      * @return  response
      * @author MahmoudGamal
      */
-    public function sendUserVerificationEmailAction($userName, $userEmail, $verificationHash) {
+    public static function sendUserVerificationEmail($userName, $userEmail, $verificationHash) {
         $body = 'Welcome to Entangle!
             
         Your account has been created, you can 
@@ -38,9 +38,10 @@ class VerificationController extends Controller {
         Username: ' . $userName . '
         ------------------------
         Please click this link to verify your account:
-        http://www.entangle.com/verify/' . $verificationHash . '';
+        http://www.entangle.io/verify/' . $verificationHash . '';
         $subject = 'Entangle user verification';
-        sendMailToEmail($userEmail, $subject, $body);
+        $notificationCenter = $this->get('notification_center.service');
+        $notificationCenter->sendMailToEmail($userEmail, $subject, $body);
         return new Response("Email Sent", 200);
     }
     /**
@@ -50,68 +51,42 @@ class VerificationController extends Controller {
      * @param String $verificationHash
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function sendEmailVerificationEmailAction($userName , $userEmail , $verificationHash){
+    public static function sendEmailVerificationEmail($userName , $userEmail , $verificationHash){
         $body = 'Hello' .$userName .'
             
                 You have added this email as an additional email to your account.
                 If you did so please click on the following link to verify:
                 
-                http://www.entangle.com/verify/email/' . $verificationHash . '
+                http://www.entangle.io/verify/' . $verificationHash . '
                 If this is not you please ignore this email';
         $subject = 'Entangle Additional Email verification';
-        sendMailToEmail($userEmail , $subject , $body);
+        $notificationCenter = $this->get('notification_center.service');
+        $notificationCenter->sendMailToEmail($userEmail, $subject, $body);
         return new Response("Email Sent",200);
-    }
-    /**
-     * This method changes the verified parameter of the user to true
-     * @param String $verificationCode
-     * @return twig view
-     * @author MahmoudGamal
-     */
-    public function verifyUserAction($verificationCode) {
-        $criteria = array('verificationCode' => $verificationCode);
-        $search = $this->getDoctrine()
-                        ->getRepository('MegasoftEntangleBundle:VerificationCode')
-                        ->findOneBy($criteria);
-        if (!$search) {
-             return $this->render('MegasoftEntangleBundle:Verified:notfound.html.twig');
-        }
-        $expired = $search->getExpired();
-        if ($expired) {
-            return $this->render('MegasoftEntangleBundle:Verified:expired.html.twig');
-        }
-        $user = $search->getUser();
-        $username = $user->getName();
-        $user->setVerified(true);
-        $search->setExpired(true);
-        $this->getDoctrine()->getManager()->flush();
-        return $this->render('MegasoftEntangleBundle:Verified:verified.html.twig',array(
-        'username' => $username));
     }
     /**
      * This method Verifies additional emails for user
      * @param String $verificationCode
      * @return twig view
      */
-    
     public function verifyEmailAction($verificationCode) {
         $criteria = array('verificationCode' => $verificationCode);
         $search = $this->getDoctrine()
                 ->getRepository('MegasoftEntangleBundle:VerificationCode')
                 ->findOneBy($criteria);
-        $UserEmail = $search->getEmail();
-        $expired = $search->getExpired();
-        $username = $search->getUser()->getName();
         if (!$search) {
            return $this->render('MegasoftEntangleBundle:Verified:notfound.html.twig');
         }
+        $UserEmail = $search->getUserEmail();
+        $expired = $search->getExpired();
         if ($expired) {
             return $this->render('MegasoftEntangleBundle:Verified:expired.html.twig');
         }
+        $username = $UserEmail->getUser()->getName();
         $UserEmail->setVerified(true);
         $search->setExpired(true);
         $this->getDoctrine()->getManager()->flush();
-        return $this->render('MegasoftEntangleBundle:Verified:emailVerified.html.twig',array(
+        return $this->render('MegasoftEntangleBundle:Verified:verified.html.twig',array(
         'username' => $username));
     }
 }
