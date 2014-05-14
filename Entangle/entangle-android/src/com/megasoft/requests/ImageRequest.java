ff5a5309e8350f173f23bd88c37cdb86b609e0b3
MohamedBassem <medoox240@gmail.com>
2014-05-14 18:45:56 +0200

    Issue #476 - Adding documentation

--- Entangle/entangle-android/src/com/megasoft/requests/ImageRequest.java
+++ Entangle/entangle-android/src/com/megasoft/requests/ImageRequest.java
@@ -5,7 +5,12 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-
+/**
+ * The class responsible for viewing the image from the cache if it is there and
+ * download it if it's a cache miss.
+ * 
+ * @author MohamedBassem
+ */
 public class ImageRequest{
 	public ImageRequest(String url,Context context,ImageView imageView){
 		Picasso.with(context).load(url).into(imageView);
