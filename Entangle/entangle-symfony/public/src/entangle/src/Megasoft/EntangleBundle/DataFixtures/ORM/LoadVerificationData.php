8d24566c394950688ae378119344a2338f2bd581
MahmoudGamalEid <mahmoudgamaleid@gmail.com>
2014-05-14 21:44:00 +0200

    Issue #404 - Add Data Fixture

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadVerificationData.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/LoadVerificationData.php
@@ -6,12 +6,14 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Megasoft\EntangleBundle\Entity\User;
+use Megasoft\EntangleBundle\Entity\VerificationCode;
+use Megasoft\EntangleBundle\Entity\UserEmail;
 
 /*
  * Fixtures for Session table
  * @author OmarElAzazy
  */
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+class LoadVerificationData extends AbstractFixture implements OrderedFixtureInterface
 {
     /**
      * {@inheritDoc}
@@ -21,17 +23,41 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $user = new User();
         $user->setName('sampleUser');
         $user->setPassword('samplePassword');
-        $verificationCode = new VerificationCode();
-        $verificationCode->setUser($user);
-        $verificationCode->setVerificationCode('123456');
+        $date = new \DateTime("now");
         $userEmail = new UserEmail();
         $userEmail->setUser($user);
-        $userEmail->setEmail('mahmoudgamaleid@gmail.com');
+        $userEmail->setEmail('sampleEmail@sample.com');
+        $verificationCode = new VerificationCode();
+        $verificationCode->setUserEmail($userEmail);
+        $verificationCode->setVerificationCode('123456');
+        $verificationCode->setCreated($date);
+        $verificationCode->setExpired(false);
+        $user2 = new User();
+        $user2->setName('sampleUser1');
+        $user2->setPassword('samplePassword');
+        $date = new \DateTime("now");
+        $userEmail2 = new UserEmail();
+        $userEmail2->setUser($user2);
+        $userEmail2->setEmail('sampleEmail1@sample.com');
+        $verificationCode2 = new VerificationCode();
+        $verificationCode2->setUserEmail($userEmail2);
+        $verificationCode2->setVerificationCode('1234567');
+        $verificationCode2->setCreated($date);
+        $verificationCode2->setExpired(true);
+
         $manager->persist($user);
         $manager->persist($verificationCode);
         $manager->persist($userEmail);
+        $manager->persist($user2);
+        $manager->persist($verificationCode2);
+        $manager->persist($userEmail2);
         $manager->flush();
         $this->addReference('sampleUser', $user);
+        $this->addReference('123456', $verificationCode);
+        $this->addReference('sampleEmail@sample.com', $userEmail);
+        $this->addReference('sampleUser1', $user2);
+        $this->addReference('1234567', $verificationCode2);
+        $this->addReference('sampleEmail1@sample.com', $userEmail2);
     }
 
     /**
