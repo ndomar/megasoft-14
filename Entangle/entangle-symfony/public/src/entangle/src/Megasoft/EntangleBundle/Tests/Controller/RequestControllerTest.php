869a9a8c293d25a536c6c6478400099e469c5000
OmarElAzazy <oa12gb@gmail.com>
2014-05-14 05:13:49 +0200

    Issue #453 - Add request controller test class

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/RequestControllerTest.php
@@ -9,66 +9,9 @@ use Megasoft\EntangleBundle\DataFixtures\ORM\LoadUserTangleData;
 use Megasoft\EntangleBundle\Tests\EntangleTestCase;
 
 /*
- * Test Class for Tangle Controller
+ * Test Class for Request Controller
  * @author OmarElAzazy
  */
-class TangleControllerTest extends EntangleTestCase
+class RequestControllerTest extends EntangleTestCase
 {
-
-    /*
-     * Test Case testing sending a wrong session to AllUsersAction
-     * @author OmarElAzazy
-     */
-    public function testAllUsersAction_WrongSession(){
-        $this->addFixture(new LoadTangleData());
-        $this->addFixture(new LoadUserData());
-        $this->addFixture(new LoadSessionData());
-        $this->addFixture(new LoadUserTangleData());
-        $this->loadFixtures();
-
-        $client = static::createClient();
-        $client->request('GET',
-            '/tangle/1/user',
-            array(),
-            array(),
-            array('HTTP_X_SESSION_ID'=>'wrongSession'));
-
-        $this->assertEquals(400, $client->getResponse()->getStatusCode());
-    }
-
-    /*
-     * Test Case testing sending correct request to AllUsersAction
-     * @author OmarElAzazy
-     */
-    public function testAllUsersAction_GetListWithSampleUser(){
-        $this->addFixture(new LoadTangleData());
-        $this->addFixture(new LoadUserData());
-        $this->addFixture(new LoadSessionData());
-        $this->addFixture(new LoadUserTangleData());
-        $this->loadFixtures();
-
-        $client = static::createClient();
-        $client->request('GET',
-            '/tangle/1/user',
-            array(),
-            array(),
-            array('HTTP_X_SESSION_ID'=>'sampleSession'));
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        $json_string = $client->getResponse()->getContent();
-        $this->assertJson($json_string);
-
-        $json = json_decode($json_string, true);
-        $this->assertEquals(2, sizeof($json));
-        $this->assertEquals(true, isset($json['count']));
-        $this->assertEquals(true, isset($json['users']));
-        $this->assertEquals(1, $json['count']);
-
-        $users = $json['users'];
-        $this->assertEquals(1, $users[0]['id']);
-        $this->assertEquals('sampleUser', $users[0]['username']);
-        $this->assertEquals(0, $users[0]['balance']);
-        $this->assertEquals('http://entangle.io/images/profilePictures/', $users[0]['iconUrl']);
-    }
 }
