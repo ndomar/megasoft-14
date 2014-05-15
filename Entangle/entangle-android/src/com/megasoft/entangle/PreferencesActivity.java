533dd687a383e42069e6019d581544f5cf3cc710
revcoder93 <revcoder93@gmail.com>
2014-05-15 05:37:07 +0200

    Issue #498 Resolving errors

--- Entangle/entangle-android/src/com/megasoft/entangle/PreferencesActivity.java
+++ Entangle/entangle-android/src/com/megasoft/entangle/PreferencesActivity.java
@@ -7,46 +7,63 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ToggleButton;
 
+/*
+ * This activity is responsible for the preferences
+ */
 public class PreferencesActivity extends Activity {
-
-	ToggleButton push;
-	ToggleButton email;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.template_preferences);
-		push = (ToggleButton) findViewById(R.id.pushNotificationsToggle);
-		email = (ToggleButton) findViewById(R.id.emailNotificationsToggle);
 
 	}
 
+	/*
+	 * This method gets fired once the push notifications toggle button is
+	 * clicked, it sets the text, color and value of the button .
+	 */
 	public void togglePushNotifications(View v) {
-		if (push.getText().equals("OFF")) {
-			push.setText("ON");
+		ToggleButton push = (ToggleButton) findViewById(R.id.pushNotificationsToggle);
+		if (push.getText().toString().equals("ON")) {
 			push.setTextColor(getResources().getColor(R.color.green));
+			push.setChecked(true);
 		} else {
-			push.setText("OFF");
 			push.setTextColor(getResources().getColor(R.color.red));
+			push.setChecked(false);
 		}
 	}
+
+	/*
+	 * This method gets fired once the email notifications toggle button is
+	 * clicked, it sets the text, color and value of the button .
+	 */
 
 	public void toggleEmailNotifications(View v) {
-		if (email.getText().equals("OFF")) {
-			email.setText("ON");
+
+		ToggleButton email = (ToggleButton) findViewById(R.id.emailNotificationsToggle);
+		if (email.getText().toString().equals("ON")) {
 			email.setTextColor(getResources().getColor(R.color.green));
+			email.setChecked(true);
 		} else {
-			email.setText("OFF");
 			email.setTextColor(getResources().getColor(R.color.red));
+			email.setChecked(false);
 		}
+	}
+
+	/*
+	 * This method is fired when the cancel button is clicked, it cancels any
+	 * changes that were done by the user .
+	 */
+	public void cancelChanges(View v) {
 
 	}
 
-	public void cancelChanges() {
-
-	}
-
-	public void changesDone() {
+	/*
+	 * This method is fired when the done button is clicked, it saves any
+	 * changes that were done by the user .
+	 */
+	public void changesDone(View v) {
 
 	}
 
