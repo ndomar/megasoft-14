067484f7e5adcfc70ea0001372136060b5260c68
silvermist <sue.fairy@live.com>
2014-05-15 03:20:46 +0200

    Issue #402 - add initial test cases

--- Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/TangleControllerTest.php
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/Tests/Controller/TangleControllerTest.php
@@ -71,4 +71,114 @@ class TangleControllerTest extends EntangleTestCase
         $this->assertEquals(0, $users[0]['balance']);
         $this->assertEquals('http://entangle.io/images/profilePictures/', $users[0]['iconUrl']);
     }
+     public function testResetTangleAction_NoSessionId() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset');
+
+        $this->assertEquals(400, $client->getResponse()->getStatusCode(), 'check for having no session id');
+    }
+
+    public function testResetAction_BadSessionId() {
+        $this->addFixture(new LoadResetTangletData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'badSessionId'));
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode(), 'Check for having bad session id');
+    }
+
+    public function testResetTangleAction_ExpiredSessionId() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'sessionUserExpired'));
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode(), 'Check for expired session id');
+    }
+
+    public function testResetTangleAction_NoSuchTangle() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/-1/reset');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode(), 'Check for tangle doesn\'t exist');
+    }
+
+    public function testResetTangleAction_UserNotOwner() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'sessionUserNotOwner'));
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode(), 'Check for user not owner');
+    }
+
+    public function testResetTangleAction_UserNotInTheTangle() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'sessionUserNotInTheTangle'));
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode(), 'Check for user not in the tangle');
+    }
+
+    public function testResetTangleAction_RequestsDeleted() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'ownerSession'));
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $tangleRepo = $this->getDoctrine()->getRepository("MegasoftEntangleBundle:Tangle");
+        $tangle = $tangleRepo->findOneBy(array('id' => 1));
+        $requests = $tangle->getRequests();
+        foreach ($requests as $request) {
+            $deleted = $request->getDeleted();
+            $this->assertEquals(1, $deleted, 'Check all requests are deleted');
+        }
+    }
+
+    public function testResetTangleAction_OffersDeleted() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'ownerSession'));
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $tangleRepo = $this->getDoctrine()->getRepository("MegasoftEntangleBundle:Tangle");
+        $tangle = $tangleRepo->findOneBy(array('id' => 1));
+        $requests = $tangle->getRequests();
+        $offers = $requests->getOffers();
+        foreach ($offers as $offer) {
+            $deleted = $offer->getDeleted();
+            $this->assertEquals(1, $deleted, 'Check all offers are deleted');
+        }
+    }
+
+    public function testResetTangleAction_ClaimsDeleted() {
+        $this->addFixture(new LoadResetTangleData());
+        $this->loadFixtures();
+
+        $client = static::createClient();
+        $client->request('PUT', '/tangle/1/reset', array(), array(), array('HTTP_X_SESSION_ID' => 'ownerSession'));
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $tangleRepo = $this->getDoctrine()->getRepository("MegasoftEntangleBundle:Tangle");
+        $tangle = $tangleRepo->findOneBy(array('id' => 1));
+        $claims = $tangle->getClaims();
+        foreach ($claims as $claim) {
+            $deleted = $claim->getDeleted();
+            $this->assertEquals(1, $deleted, 'Check all claims are deleted');
+        }
+    }
+    
+
 }
