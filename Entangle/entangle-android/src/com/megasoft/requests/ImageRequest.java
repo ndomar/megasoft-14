8cfdd813bcf96a070c22770aaeb0b682e8569aea
Alaa Maher <almgohar@gmail.com>
2014-05-16 12:53:06 +0200

    Issue #492 - fix frontend image bug

--- Entangle/entangle-android/src/com/megasoft/requests/ImageRequest.java
+++ Entangle/entangle-android/src/com/megasoft/requests/ImageRequest.java
@@ -12,43 +12,46 @@ import android.os.AsyncTask;
 import android.widget.ImageView;
 
 /**
- * This code references the second answer from 
- * http://stackoverflow.com/questions/15217436/image-view-image-from-imageurl-giving-exception
+ * This code references the second answer from
+ * http://stackoverflow.com/questions
+ * /15217436/image-view-image-from-imageurl-giving-exception
  */
 public class ImageRequest extends AsyncTask<String, Void, Bitmap> {
-	    private final WeakReference<ImageView> imageViewReference;
-	    private String data;
+	private final WeakReference<ImageView> imageViewReference;
+	private String data;
 
-	    public ImageRequest(ImageView imageView) {
-	        // Use a WeakReference to ensure the ImageView can be garbage
-	        // collected
-	        imageViewReference = new WeakReference<ImageView>(imageView);
-	    }
-
-	    // Decode image in background.
-	    @Override
-	    protected Bitmap doInBackground(String... params) {
-	        data = params[0];
-	        try {
-	            return BitmapFactory.decodeStream((InputStream) new URL(data)
-	                    .getContent());
-	        } catch (MalformedURLException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        return null;
-	    }
-
-	    // Once complete, see if ImageView is still around and set bitmap.
-	    @Override
-	    protected void onPostExecute(Bitmap bitmap) {
-	        if (imageViewReference != null && bitmap != null) {
-	            final ImageView imageView = imageViewReference.get();
-	            if (imageView != null) {
-	                imageView.setImageBitmap(bitmap);
-	            }
-	        }
-	    }
+	public ImageRequest(ImageView imageView) {
+		// Use a WeakReference to ensure the ImageView can be garbage
+		// collected
+		imageViewReference = new WeakReference<ImageView>(imageView);
 	}
 
+	// Decode image in background.
+	@Override
+	protected Bitmap doInBackground(String... params) {
+		data = params[0];
+		try {
+			if (data == null) {
+				return null;
+			}
+			return BitmapFactory.decodeStream((InputStream) new URL(data)
+					.getContent());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	// Once complete, see if ImageView is still around and set bitmap.
+	@Override
+	protected void onPostExecute(Bitmap bitmap) {
+		if (imageViewReference != null && bitmap != null) {
+			final ImageView imageView = imageViewReference.get();
+			if (imageView != null) {
+				imageView.setImageBitmap(bitmap);
+			}
+		}
+	}
+}
