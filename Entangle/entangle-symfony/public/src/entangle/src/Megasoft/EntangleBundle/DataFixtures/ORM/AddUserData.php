62f9a2f4c47130030f744cad094865ad0c76004c
Eslamunto <eslam.shala@gmail.com>
2014-05-16 18:23:40 +0200

    Issue#426 - Add my fixture

---
+++ Entangle/entangle-symfony/public/src/entangle/src/Megasoft/EntangleBundle/DataFixtures/ORM/AddUserData.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neuron
+ * Date: 5/16/14
+ * Time: 5:00 PM
+ */
+
+namespace Megasoft\EntangleBundle\DataFixtures\ORM;
+
+
+use Megasoft\EntangleBundle\Entity\User;
+use Megasoft\EntangleBundle\Entity\UserEmail;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AddUserData extends AbstractFixture implements OrderedFixtureInterface {
+
+    public function load (ObjectManager $manager) {
+        $user = new User();
+        $userEmail = new UserEmail();
+        $user->setName("Zanaty");
+        $userEmail->setEmail("zanaty@gmail.com");
+        $user->setPassword('1234567890');
+
+        $manager->persist($user);
+        $manager->flush();
+
+
+    }
+
+    public function getOrder()
+    {
+        return 5;
+    }
+}
